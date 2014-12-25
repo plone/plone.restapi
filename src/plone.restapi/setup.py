dd2c4dfcf26783c19696c4bedb80fb3ce14262f4
Timo Stollenwerk <tisto@plone.org>
2014-12-25 06:17:31 +0100

    Pep8.

--- src/plone.restapi/setup.py
+++ src/plone.restapi/setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import os
 
 version = '0.1'
 
@@ -21,17 +20,17 @@ setup(name='plone.restapi',
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      author='Timo Stollenwerk',
+      author_email='tisto@plone.org',
+      url='https://github.com/plone/plone.restapi/',
       license='gpl',
       packages=find_packages('src'),
       package_dir={'': 'src'},
