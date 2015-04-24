15f85de7e594cda68e69c84f67a412d5700ac277
Timo Stollenwerk <tisto@plone.org>
2015-04-24 12:04:24 +0200

    Add get_object_schema tests for news item and image. Ignore schema order.

--- src/plone/restapi/tests/test_utils.py
+++ src/plone/restapi/tests/test_utils.py
@@ -22,8 +22,8 @@ class GetObjectSchemaUnitTest(unittest.TestCase):
         self.portal.invokeFactory('Document', id='doc1', title='Doc 1')
         schema = [x[0] for x in get_object_schema(self.portal.doc1)]
         self.assertEqual(
-            schema,
-            [
+            set(schema),
+            set([
                 'text',
                 'title',
                 'allow_discussion',
@@ -42,15 +42,77 @@ class GetObjectSchemaUnitTest(unittest.TestCase):
                 'creators',
                 'description',
                 'changeNote'
-            ]
+            ])
+        )
+
+    def test_get_object_schema_for_news_item(self):
+        self.portal.invokeFactory(
+            'News Item',
+            id='newsitem1',
+            title='News Item 1'
+        )
+        schema = [x[0] for x in get_object_schema(self.portal.newsitem1)]
+        self.assertEqual(
+            set(schema),
+            set([
+                'text',
+                'title',
+                'allow_discussion',
+                'exclude_from_nav',
+                'relatedItems',
+                'image',
+                'image_caption',
+                'meta_type',
+                'isPrincipiaFolderish',
+                'icon',
+                'rights',
+                'contributors',
+                'effective',
+                'expires',
+                'language',
+                'subjects',
+                'creators',
+                'description',
+                'changeNote'
+            ])
+        )
+
+    def test_get_object_schema_for_image(self):
+        self.portal.invokeFactory(
+            'Image',
+            id='image1',
+            title='Image 1'
+        )
+        schema = [x[0] for x in get_object_schema(self.portal.image1)]
+        self.assertEqual(
+            set(schema),
+            set([
+                'image',
+                'description',
+                'title',
+                'allow_discussion',
+                'exclude_from_nav',
+                'relatedItems',
+                'meta_type',
+                'isPrincipiaFolderish',
+                'icon',
+                'rights',
+                'effective',
+                'expires',
+                'language',
+                'subjects',
+                'creators',
+                'contributors',
+                'rights',
+            ])
         )
 
     def test_get_object_schema_for_folder(self):
         self.portal.invokeFactory('Folder', id='folder1', title='Folder 1')
         schema = [x[0] for x in get_object_schema(self.portal.folder1)]
         self.assertEqual(
-            schema,
-            [
+            set(schema),
+            set([
                 'title',
                 'allow_discussion',
                 'exclude_from_nav',
@@ -69,7 +131,7 @@ class GetObjectSchemaUnitTest(unittest.TestCase):
                 'subjects',
                 'creators',
                 'description'
-            ]
+            ])
         )
 
 
