5b9b00789406cac53c79f8a22cf29eca67e7e138
Timo Stollenwerk <tisto@plone.org>
2015-07-23 23:02:24 +0200

    Rename adapter -> serializer. Clean up tests.

--- src/plone/restapi/tests/test_serializer.py
+++ src/plone/restapi/tests/test_serializer.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
-import unittest2 as unittest
-
 from plone.restapi.interfaces import ISerializeToJson
 from plone.app.textfield.value import RichTextValue
-
 from plone.restapi.testing import PLONE_RESTAPI_INTEGRATION_TESTING
 from plone.namedfile.file import NamedBlobImage
 from DateTime import DateTime
 
 import os
+import unittest
 
 
 class TestSerializeToJsonAdapter(unittest.TestCase):
@@ -21,19 +19,19 @@ class TestSerializeToJsonAdapter(unittest.TestCase):
         self.portal_url = self.portal.absolute_url()
         self.portal.invokeFactory('Document', id='doc1', title='Document 1')
 
-    def test_serialize_to_json_adapter_returns_context(self):
+    def test_serialize_returns_context(self):
         self.assertEqual(
             ISerializeToJson(self.portal.doc1)['@context'],
             u'http://www.w3.org/ns/hydra/context.jsonld'
         )
 
-    def test_serialize_to_json_adapter_returns_id(self):
+    def test_serialize_returns_id(self):
         self.assertEqual(
             ISerializeToJson(self.portal.doc1)['@id'],
             self.portal_url + '/doc1'
         )
 
-    def test_serialize_to_json_adapter_returns_type(self):
+    def test_serialize_returns_type(self):
         self.assertTrue(
             ISerializeToJson(self.portal.doc1).get('@type'),
             'The @type attribute should be present.'
@@ -43,20 +41,20 @@ class TestSerializeToJsonAdapter(unittest.TestCase):
             u'Document'
         )
 
-    def test_serialize_to_json_adapter_returns_title(self):
+    def test_serialize_returns_title(self):
         self.assertEqual(
             ISerializeToJson(self.portal.doc1)['title'],
             u'Document 1'
         )
 
-    def test_serialize_to_json_adapter_returns_desciption(self):
+    def test_serialize_returns_desciption(self):
         self.portal.doc1.description = u'This is a document'
         self.assertEqual(
             ISerializeToJson(self.portal.doc1)['description'],
             u'This is a document'
         )
 
-    def test_serialize_to_json_adapter_returns_rich_text(self):
+    def test_serialize_returns_rich_text(self):
         self.portal.doc1.text = RichTextValue(
             u"Lorem ipsum.",
             'text/plain',
@@ -67,21 +65,21 @@ class TestSerializeToJsonAdapter(unittest.TestCase):
             u'<p>Lorem ipsum.</p>'
         )
 
-    def test_serialize_to_json_adapter_returns_effective(self):
+    def test_serialize_returns_effective(self):
         self.portal.doc1.setEffectiveDate(DateTime('2014/04/04'))
         self.assertEqual(
             ISerializeToJson(self.portal.doc1)['effective'],
             '2014-04-04T00:00:00'
         )
 
-    def test_serialize_to_json_adapter_returns_expires(self):
+    def test_serialize_returns_expires(self):
         self.portal.doc1.setExpirationDate(DateTime('2017/01/01'))
         self.assertEqual(
             ISerializeToJson(self.portal.doc1)['expires'],
             '2017-01-01T00:00:00'
         )
 
-    def test_serialize_to_json_adapter_on_folder_returns_member_attr(self):
+    def test_serialize_on_folder_returns_member_attr(self):
         self.portal.invokeFactory('Folder', id='folder1', title='Folder 1')
         self.portal.folder1.invokeFactory('Document', id='doc1')
         self.portal.folder1.doc1.title = u'Document 1'
@@ -97,7 +95,7 @@ class TestSerializeToJsonAdapter(unittest.TestCase):
             ]
         )
 
-    def test_serialize_to_json_adapter_returns_parent(self):
+    def test_serialize_returns_parent(self):
         self.assertTrue(
             ISerializeToJson(self.portal.doc1).get('parent'),
             'The parent attribute should be present.'
@@ -111,7 +109,7 @@ class TestSerializeToJsonAdapter(unittest.TestCase):
             ISerializeToJson(self.portal.doc1)['parent']
         )
 
-    def test_serialize_to_json_adapter_does_not_returns_parent_on_root(self):
+    def test_serialize_does_not_returns_parent_on_root(self):
         self.assertEqual(
             {},
             ISerializeToJson(self.portal).get('parent'),
@@ -127,7 +125,7 @@ class TestSerializeToJsonAdapter(unittest.TestCase):
             'The parent attribute on portal root should be None'
         )
 
-    def test_serialize_to_json_adapter_returns_site_root_type(self):
+    def test_serialize_returns_site_root_type(self):
         self.assertTrue(
             ISerializeToJson(self.portal).get('@type'),
             'The @type attribute should be present.'
@@ -137,7 +135,7 @@ class TestSerializeToJsonAdapter(unittest.TestCase):
             u'SiteRoot'
         )
 
-    def test_serialize_to_json_adapter_ignores_underscore_values(self):
+    def test_serialize_ignores_underscore_values(self):
         self.assertFalse(
             '__name__' in ISerializeToJson(self.portal.doc1)
         )
@@ -145,14 +143,14 @@ class TestSerializeToJsonAdapter(unittest.TestCase):
             'manage_options' in ISerializeToJson(self.portal.doc1)
         )
 
-    def test_serialize_to_json_adapter_file(self):
+    def test_serialize_file(self):
         self.portal.invokeFactory('File', id='file1', title='File 1')
         self.assertEqual(
             '{0}/@@download'.format(self.portal.file1.absolute_url()),
             ISerializeToJson(self.portal.file1).get('download')
         )
 
-    def test_serialize_to_json_adapter_image(self):
+    def test_serialize_image(self):
         self.portal.invokeFactory('Image', id='image1', title='Image 1')
         image_file = os.path.join(os.path.dirname(__file__), u'image.png')
         self.portal.image1.image = NamedBlobImage(
