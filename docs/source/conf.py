585a1d0c9c60d62199b2ce5910802949c0ee3f8b
Timo Stollenwerk <tisto@plone.org>
2016-05-16 11:57:03 +0200

    Ignore ideas folder in docs to avoid sphinx warnings.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,7 +66,7 @@ release = '1.0a1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['*ideas/*']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
