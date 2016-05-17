9aca03245054ec06b9c0276143ccec2ec40f30f3
Victor Fernandez de Alba <sneridagh@gmail.com>
2016-05-17 18:54:48 +0200

    Add registry endpoint implementation

--- src/plone/restapi/tests/test_documentation.py
+++ src/plone/restapi/tests/test_documentation.py
@@ -281,3 +281,14 @@ class TestTraversal(unittest.TestCase):
         response = self.api_session.post(
             '{}/workflow/publish'.format(self.document.absolute_url()))
         save_response_for_documentation('workflow_post.json', response)
+
+    def test_documentation_registry_get(self):
+        response = self.api_session.get(
+            '/@registry/plone.app.querystring.field.path.title')
+        save_response_for_documentation('registry_get.json', response)
+
+    def test_documentation_registry_update(self):
+        response = self.api_session.patch(
+            '/@registry/',
+            json={'plone.app.querystring.field.path.title': 'Value'})
+        save_response_for_documentation('registry_update.json', response)
---
+++ src/plone/restapi/tests/test_registry.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from plone.app.testing import setRoles
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.app.testing import TEST_USER_ID
+from plone.registry import field
+from plone.registry.interfaces import IRegistry
+from plone.registry.record import Record
+from plone.restapi.testing import PLONE_RESTAPI_DX_FUNCTIONAL_TESTING
+from plone.restapi.testing import RelativeSession
+from zope.component import getUtility
+
+import transaction
+import unittest
+
+
+class TestRegistry(unittest.TestCase):
+
+    layer = PLONE_RESTAPI_DX_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.app = self.layer['app']
+        self.portal = self.layer['portal']
+        self.portal_url = self.portal.absolute_url()
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+        self.api_session = RelativeSession(self.portal_url)
+        self.api_session.headers.update({'Accept': 'application/json'})
+        self.api_session.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
+
+        registry = getUtility(IRegistry)
+        record = Record(field.TextLine(title=u"Foo Bar"), u"Lorem Ipsum")
+        registry.records['foo.bar'] = record
+        transaction.commit()
+
+    def test_get_registry_record(self):
+        response = self.api_session.get('/@registry/foo.bar')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), 'Lorem Ipsum')
+
+    def test_update_registry_record(self):
+        registry = getUtility(IRegistry)
+        payload = {'foo.bar': 'lorem ipsum'}
+        response = self.api_session.patch('/@registry', json=payload)
+        transaction.commit()
+
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(registry['foo.bar'], 'lorem ipsum')
+
+    def test_update_several_registry_records(self):
+        registry = getUtility(IRegistry)
+        record = Record(field.TextLine(title=u"Foo Bar Baz"), u"Lorem Ipsum Dolor")
+        registry.records['foo.bar.baz'] = record
+        transaction.commit()
+        payload = {'foo.bar': 'lorem ipsum',
+                   'foo.bar.baz': 'lorem ipsum dolor'}
+        response = self.api_session.patch('/@registry', json=payload)
+        transaction.commit()
+
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(registry['foo.bar'], 'lorem ipsum')
+        self.assertEqual(registry['foo.bar.baz'], 'lorem ipsum dolor')
+
+    def test_update_non_existing_registry_record(self):
+        payload = {'foo.bar.baz': 'lorem ipsum'}
+        response = self.api_session.patch('/@registry', json=payload)
+        self.assertEqual(response.status_code, 500)
