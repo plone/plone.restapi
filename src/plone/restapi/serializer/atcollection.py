a30b6671957ecfe2d1c891de2105b2cbe976d3ab
Mikel Larreategi <mlarreategi@codesyntax.com>
2016-05-25 15:08:59 +0200

    serialize plone.app.collection

---
+++ src/plone/restapi/serializer/atcollection.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from plone.app.collection.interfaces import ICollection
+from plone.restapi.batching import HypermediaBatch
+from plone.restapi.interfaces import ISerializeToJson
+from plone.restapi.interfaces import ISerializeToJsonSummary
+from plone.restapi.serializer.atcontent import SerializeToJson
+from zope.component import adapter
+from zope.component import getMultiAdapter
+from zope.interface import implementer
+from zope.interface import Interface
+
+
+@implementer(ISerializeToJson)
+@adapter(ICollection, Interface)
+class SerializeCollectionToJson(SerializeToJson):
+
+    def __call__(self):
+        collection_metadata = super(SerializeCollectionToJson, self).__call__()
+        results = self.context.results(batch=False)
+        batch = HypermediaBatch(self.context, self.request, results)
+
+        results = collection_metadata
+        results['@id'] = batch.canonical_url
+        results['items_total'] = batch.items_total
+        if batch.links:
+            results['batching'] = batch.links
+
+        results['items'] = [
+            getMultiAdapter((brain, self.request), ISerializeToJsonSummary)()
+            for brain in batch
+        ]
+        return results
