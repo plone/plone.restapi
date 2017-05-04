8dc3103766d619fbbe6f24e7b841505438ec68f4
Roel Bruggink <roel@jaroel.nl>
2017-05-04 11:05:48 +0200

    Add registry based control panels to REST API

--- src/plone/restapi/controlpanels.py
+++ src/plone/restapi/controlpanels.py
@@ -3,8 +3,17 @@ from zope.interface import implementer, Interface, Attribute
 from zope.component import adapter
 from Products.CMFCore.utils import getToolByName
 
-from Products.CMFPlone.interfaces import IEditingSchema
+from Products.CMFPlone.interfaces.controlpanel import IDateAndTimeSchema
+from Products.CMFPlone.interfaces.controlpanel import IEditingSchema
+from Products.CMFPlone.interfaces.controlpanel import ILanguageSchema
 from Products.CMFPlone.interfaces.controlpanel import IMailSchema
+from Products.CMFPlone.interfaces.controlpanel import INavigationSchema
+from Products.CMFPlone.interfaces.controlpanel import ISiteSchema
+from Products.CMFPlone.interfaces.controlpanel import ISearchSchema
+from Products.CMFPlone.interfaces.controlpanel import ISocialMediaSchema
+from Products.CMFPlone.interfaces.controlpanel import IImagingSchema
+from Products.CMFPlone.interfaces.controlpanel import IMarkupSchema
+from Products.CMFPlone.interfaces.controlpanel import ISecuritySchema
 
 
 class IControlpanel(Interface):
@@ -53,17 +62,84 @@ class RegistryConfigletPanel(object):
             self.group = self._get_group_title()
 
 
-@implementer(IControlpanel)
+# General
+
+@adapter(Interface, Interface)
+class DateTimeControlpanel(RegistryConfigletPanel):
+    schema = IDateAndTimeSchema
+    configlet_id = 'DateAndTime'
+    configlet_category_id = 'plone-general'
+
+
+@adapter(Interface, Interface)
+class LanguageControlpanel(RegistryConfigletPanel):
+    schema = ILanguageSchema
+    configlet_id = 'LanguageSettings'
+    configlet_category_id = 'plone-general'
+
+
+@adapter(Interface, Interface)
+class MailControlpanel(RegistryConfigletPanel):
+    schema = IMailSchema
+    configlet_id = 'MailHost'
+    configlet_category_id = 'plone-general'
+
+
+@adapter(Interface, Interface)
+class NavigationControlpanel(RegistryConfigletPanel):
+    schema = INavigationSchema
+    configlet_id = 'NavigationSettings'
+    configlet_category_id = 'plone-general'
+
+
+@adapter(Interface, Interface)
+class SiteControlpanel(RegistryConfigletPanel):
+    schema = ISiteSchema
+    configlet_id = 'PloneReconfig'
+    configlet_category_id = 'plone-general'
+
+
+@adapter(Interface, Interface)
+class SearchControlpanel(RegistryConfigletPanel):
+    schema = ISearchSchema
+    configlet_id = 'SearchSettings'
+    configlet_category_id = 'plone-general'
+
+
+@adapter(Interface, Interface)
+class SocialMediaControlpanel(RegistryConfigletPanel):
+    schema = ISocialMediaSchema
+    configlet_id = 'socialmedia'
+    configlet_category_id = 'plone-general'
+
+
+# Content
+
 @adapter(Interface, Interface)
 class EditingControlpanel(RegistryConfigletPanel):
-    registry_schema = IEditingSchema
+    schema = IEditingSchema
     configlet_id = 'EditingSettings'
     configlet_category_id = 'plone-content'
 
 
-@implementer(IControlpanel)
 @adapter(Interface, Interface)
-class MailControlpanel(RegistryConfigletPanel):
-    registry_schema = IMailSchema
-    configlet_id = 'MailHost'
-    configlet_category_id = 'plone-general'
+class ImagingControlpanel(RegistryConfigletPanel):
+    schema = IImagingSchema
+    configlet_id = 'ImagingSettings'
+    configlet_category_id = 'plone-content'
+
+
+@adapter(Interface, Interface)
+class MarkupControlpanel(RegistryConfigletPanel):
+    schema = IMarkupSchema
+    configlet_id = 'MarkupSettings'
+    configlet_category_id = 'plone-content'
+
+
+# Security
+
+@adapter(Interface, Interface)
+class SecurityControlpanel(RegistryConfigletPanel):
+    schema = ISecuritySchema
+    configlet_id = 'SecuritySettings'
+    configlet_category_id = 'plone-security'
