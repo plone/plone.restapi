ca39148ece50edcc5665854ac05d7f597476db53
Roel Bruggink <roel@jaroel.nl>
2017-11-30 15:43:46 +0100

    Remove failing test that shouldn't have passed in the first place as the image field is required.
    We do test this behavior in the dxfield deserializer.

--- src/plone/restapi/tests/test_content_patch.py
+++ src/plone/restapi/tests/test_content_patch.py
@@ -79,39 +79,6 @@ class TestContentPatch(unittest.TestCase):
         )
         self.assertEqual(401, response.status_code)
 
-    def test_patch_image_with_null_image_dont_fail_and_unsets_image(self):
-        response = requests.post(
-            self.portal.absolute_url(),
-            headers={'Accept': 'application/json'},
-            auth=(SITE_OWNER_NAME, SITE_OWNER_PASSWORD),
-            json={
-                '@type': 'Image',
-                'image': {
-                    'data': u'R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs=',  # noqa
-                    'encoding': u'base64',
-                    'content-type': u'image/gif',
-                }
-            },
-        )
-        transaction.commit()
-
-        response = response.json()
-        image_url = self.portal[response['id']].absolute_url()
-        response = requests.patch(
-            image_url,
-            headers={'Accept': 'application/json'},
-            auth=(SITE_OWNER_NAME, SITE_OWNER_PASSWORD),
-            json={
-                'image': False
-            }
-        )
-        transaction.commit()
-        response = requests.get(
-            image_url,
-            headers={'Accept': 'application/json'})
-
-        self.assertIsNone(response.json()['image'])
-
     def test_patch_image_with_the_contents_of_the_get_preserves_image(self):
         response = requests.post(
             self.portal.absolute_url(),
