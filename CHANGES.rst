c9f947419becc49b35cfddaa1e76ac5ed170e015
Timo Stollenwerk <tisto@plone.org>
2018-01-24 11:11:12 +0100

    Preparing release 1.1.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.1.0 (unreleased)
+1.1.0 (2018-01-24)
 ------------------
 
 New Features:
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.0.1.dev0'
+version = '1.1.0'
 
 long_description = (
     open('README.rst').read() + '\n' +
