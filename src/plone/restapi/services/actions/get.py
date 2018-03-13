58deaeda665e05017fa63972cc1a37b52759e442
Carsten Senger <senger@rehfisch.de>
2018-03-13 18:25:01 +0100

    @actions: Remove the translate parameter as the frontend can omit the language header

--- src/plone/restapi/services/actions/get.py
+++ src/plone/restapi/services/actions/get.py
@@ -6,11 +6,6 @@ from zope.i18n import translate
 
 class ActionsGet(Service):
 
-    def translate(self, msg):
-        if self.request.get('translate'):
-            return translate(msg, context=self.request)
-        return msg
-
     def reply(self):
         context_state = api.content.get_view(name='plone_context_state',
                                              context=self.context,
@@ -21,7 +16,7 @@ class ActionsGet(Service):
             actions = context_state.actions(category=category)
             for action in actions:
                 category_action_data.append({
-                    'title': self.translate(action['title']),
+                    'title': translate(action['title'], context=self.request),
                     'id': action['id'],
                     'icon': action['icon'],
                     })
