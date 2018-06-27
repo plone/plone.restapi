51bcb219d459466b66297b7b7dcf7328bc65ed16
Lukas Graf <lukas.graf@4teamwork.ch>
2018-06-27 11:52:28 +0200

    Dynamically include HTTP examples in data_files in setup.py:
    We need to do this dynamically because the data_files argument to
    setup() doesn't support globs (wildcards).
    
    The examples need to be included via data_files because they are outside
    a Python package. So we can't include them using package_data, which only
    works relative to Python packages. (The MANIFEST only controls what gets
    included in the source distribution. Listing these files in data_files
    ensures they actually get copied to the installed .egg).

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+import os
+
 version = '3.1.1.dev0'
 
 long_description = (
@@ -9,6 +11,30 @@ long_description = (
     open('CONTRIBUTORS.rst').read() + '\n' +
     open('CHANGES.rst').read() + '\n'
 )
+
+
+HTTP_EXAMPLES_PATH = 'docs/source/_json/'
+
+
+def collect_http_examples():
+    """Gather relative paths to every HTTP example file.
+
+    We need to do this dynamically because the data_files argument to
+    setup() doesn't support globs (wildcards).
+
+    If the HTTP examples directory is ever moved, the HTTP_EXAMPLES_PATH
+    above needs to be updated before a new release is cut.
+
+    The examples need to be included via data_files because they are outside
+    a Python package. So we can't include them using package_data, which only
+    works relative to Python packages. (The MANIFEST only controls what gets
+    included in the source distribution. Listing these files in data_files
+    ensures they actually get copied to the installed .egg).
+    """
+    examples_path = HTTP_EXAMPLES_PATH
+    example_filenames = os.listdir(examples_path)
+    return [os.path.join(examples_path, fn) for fn in example_filenames]
+
 
 setup(name='plone.restapi',
       version=version,
@@ -36,6 +62,9 @@ setup(name='plone.restapi',
       license='gpl',
       packages=find_packages('src'),
       package_dir={'': 'src'},
+      data_files=[
+          (HTTP_EXAMPLES_PATH, collect_http_examples()),
+      ],
       namespace_packages=['plone', ],
       include_package_data=True,
       zip_safe=False,
