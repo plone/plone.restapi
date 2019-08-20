39e5fea40893f009a5320570a738d248de50793b
Uwe Kretschmer <kretschmer@saw-leipzig.de>
2019-08-20 16:37:43 +0200

    Optimized relations service by using catalog results instead of objects.

--- src/plone/restapi/services/relations/get.py
+++ src/plone/restapi/services/relations/get.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
-from plone.restapi.interfaces import IExpandableElement
+from Acquisition import aq_inner
+from plone import api
+from plone.restapi.interfaces import IExpandableElement, ISerializeToJsonSummary
 from plone.restapi.services import Service
 from z3c.relationfield.interfaces import IHasRelations
+from zc.relation.interfaces import ICatalog
 from zope.component import adapter
 from zope.component import getMultiAdapter
+from zope.component import getUtility
 from zope.interface import implementer
 from zope.interface import Interface
-
-from Acquisition import aq_inner
-from zope.component import getUtility
 from zope.intid.interfaces import IIntIds
-from zope.security import checkPermission
-from zc.relation.interfaces import ICatalog
-from plone.restapi.interfaces import ISerializeToJsonSummary
 
 
 @implementer(IExpandableElement)
@@ -32,23 +30,28 @@ class Relations(object):
             "outgoing": {}
         }
 
+        portal_catalog = api.portal.get_tool('portal_catalog')
         catalog = getUtility(ICatalog)
         intids = getUtility(IIntIds)
         intid = intids.getId(aq_inner(self.context))
         for relation_type in data.keys():
             if relation_type == "incoming":
                 query = dict(to_id=intid)
+                direction = "from"
             else:
                 query = dict(from_id=intid)
+                direction = "to"
             for rel in catalog.findRelations(query):
-                if relation_type == "incoming":
-                    obj = intids.queryObject(rel.from_id)
-                else:
-                    obj = intids.queryObject(rel.to_id)
-
-                if obj is not None and checkPermission('zope2.View', obj):
+                if rel.isBroken():
+                    # skip broken relations
+                    continue
+                # query by path so we don't have to wake up any objects. Also this
+                # ensures security check.
+                brains = portal_catalog(path={'query': getattr(rel, direction + '_path'), 'depth': 0})
+                # get summary representation from brain
+                if brains is not None and len(brains):
                     summary = getMultiAdapter(
-                        (obj, self.request), ISerializeToJsonSummary
+                        (brains[0], self.request), ISerializeToJsonSummary
                     )()
                     if rel.from_attribute in data[relation_type]:
                         data[relation_type][rel.from_attribute].append(summary)
