df84d58f79c60332928e3fd8b9d971a1e479f018
Franco Pellegrini <frapell@gmail.com>
2019-10-11 13:07:39 -0300

    Add POST support for portlets

--- src/plone/restapi/services/portlets/get.py
+++ src/plone/restapi/services/portlets/get.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-from plone.portlets.interfaces import IPortletManager
 from plone.restapi.interfaces import IJsonCompatible
 from plone.restapi.interfaces import ISerializeToJson
 from plone.restapi.interfaces import ISerializeToJsonSummary
 from plone.restapi.services import Service
-from zope.component import getUtilitiesFor
-from zope.component import getUtility
+from plone.restapi.services.portlets.utils import get_portletmanagers
+from plone.restapi.services.portlets.utils import manager_by_name
 from zope.component import queryMultiAdapter
 from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
@@ -20,14 +19,6 @@ class PortletsGet(Service):
             self.portletmanager_id = name
         return self
 
-    def get_portletmanagers(self):
-        return getUtilitiesFor(IPortletManager)
-
-    def manager_by_name(self, name):
-        return getUtility(IPortletManager,
-                          name=name,
-                          context=self.context)
-
     def reply(self):
         if self.portletmanager_id:
             return self.reply_portletmanager()
@@ -39,11 +30,11 @@ class PortletsGet(Service):
                    ISerializeToJsonSummary)
                 yield serializer()
 
-        portletmanagers = self.get_portletmanagers()
+        portletmanagers = get_portletmanagers()
         return IJsonCompatible(list(serialize(portletmanagers)))
 
     def reply_portletmanager(self):
-        manager = self.manager_by_name(self.portletmanager_id)
+        manager = manager_by_name(self.context, self.portletmanager_id)
         if manager is None:
             self.request.response.setStatus(404)
             return
