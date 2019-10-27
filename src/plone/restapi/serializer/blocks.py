abc092c11ae6065cb28d3a72b9d54e8d3d5ca77a
Thomas Buchberger <t.buchberger@4teamwork.ch>
2019-10-27 11:25:04 +0100

    Resolve paths to UIDs
    On deserialization resolve paths UIDs.
    On serialization resolve UIDs to paths.
    
    Closes ##808

---
+++ src/plone/restapi/serializer/blocks.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from plone.outputfilters.browser.resolveuid import uuidToURL
+from plone.restapi.behaviors import ITiles
+from plone.restapi.interfaces import IFieldSerializer
+from plone.restapi.serializer.converters import json_compatible
+from plone.restapi.serializer.dxfields import DefaultFieldSerializer
+from plone.schema import IJSONField
+from zope.component import adapter
+from zope.interface import implementer
+from zope.interface import Interface
+import re
+
+
+RESOLVEUID_RE = re.compile('^[./]*resolve[Uu]id/([^/]*)/?(.*)$')
+
+
+@adapter(IJSONField, ITiles, Interface)
+@implementer(IFieldSerializer)
+class BlocksJSONFieldSerializer(DefaultFieldSerializer):
+    def __call__(self):
+        value = self.get_value()
+
+        # Resolve UID links
+        if self.field.getName() == 'tiles':
+            for tile in value.values():
+                if tile.get('@type') == 'text':
+                    entity_map = tile.get('text', {}).get('entityMap', {})
+                    for entity in entity_map.values():
+                        if entity.get('type') == 'LINK':
+                            href = entity.get('data', {}).get('href', '')
+                            if href:
+                                match = RESOLVEUID_RE.match(href)
+                                if match is not None:
+                                    uid, suffix = match.groups()
+                                    href = uuidToURL(uid)
+                                    if href is None:
+                                        continue
+                                    if suffix:
+                                        href += '/' + suffix
+                                    entity['data']['href'] = href
+                                    entity['data']['url'] = href
+        return json_compatible(self.get_value())
