ccf56926b286e62e38e1eb85e3e370f5dddfd80b
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-12-11 13:36:19 +0200

    Reuse navtree portlet renderer

--- src/plone/restapi/serializer/portlet.py
+++ src/plone/restapi/serializer/portlet.py
@@ -1,22 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# from Acquisition import aq_parent
-# from OFS.interfaces import IApplication
-# from plone.restapi.interfaces import IExpandableElement
-# from plone.restapi.services import Service
-# from Products.CMFPlone.browser.interfaces import INavigationTabs
-# from Products.Five import BrowserView
-
 from Acquisition import aq_base
-from Acquisition import aq_inner
 from plone import api
-from plone.api import content
-from plone.app.layout.navigation.navtree import buildFolderTree
-from plone.app.layout.navigation.root import getNavigationRoot
 from plone.app.portlets.interfaces import IPortletTypeInterface
-from plone.app.portlets.navigation import Renderer
+from plone.app.portlets.portlets.navigation import Renderer
 from plone.app.textfield.interfaces import IRichText
-from plone.app.uuid.utils import uuidToObject
 from plone.memoize import forever
 from plone.portlets.interfaces import IPortletAssignment
 from plone.portlets.interfaces import IPortletDataProvider
@@ -29,18 +17,13 @@ from plone.restapi.interfaces import ISerializeToJson
 from plone.restapi.interfaces import ISerializeToJsonSummary
 from plone.restapi.serializer.converters import json_compatible
 from plone.restapi.serializer.dxfields import DefaultFieldSerializer
-from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone import utils
-from Products.CMFPlone.browser.navtree import NavtreeQueryBuilder
-from Products.CMFPlone.browser.navtree import SitemapNavtreeStrategy
-from Products.CMFPlone.interfaces import INavigationSchema
 from ZODB.POSException import ConflictError
 from zope.component import adapter
 from zope.component import getMultiAdapter
 from zope.component import getUtilitiesFor
 from zope.component import getUtility
 from zope.component import queryMultiAdapter
-from zope.component.hooks import getSite
 from zope.interface import implementer
 from zope.interface import Interface
 from zope.interface import providedBy
@@ -148,6 +131,10 @@ class PortletManagerSerializer(object):
 
             # To be able to customize serializers per portlet type, we try to
             # lookup for a named adapter first. The name is the portlet type
+
+            # TODO: the serializer should ideally have the same discriminators
+            # as the portlet renderer: context, request, view=service, manager,
+            # data.
             type_, schema = get_portlet_info(assignment)
             serializer = queryMultiAdapter(
                 (assignment, self.context, self.request),
@@ -287,116 +274,17 @@ class NavigationPortletSerializer(PortletSerializer):
 
     def __call__(self):
         res = super(NavigationPortletSerializer, self).__call__()
+        renderer = NavtreePortletRenderer(
+            self.context,
+            self.request,
+            None,
+            None,
+            self.assignment
+        )
 
-        root = self.context
-
-        # if self.assignment.root_uid:
-        #     root = uuidToObject(self.assignment.root_uid)
-
-        # nav = PortletNavigation(root, self.request)
-        # res['navtree'] = nav(
-        #     depth=self.assignment.bottomLevel,
-        #     includeTop=self.assignment.includeTop,
-        #     currentFolderOnly=self.assignment.currentFolderOnly,
-        # )
+        res['navigationportlet'] = renderer.render()
 
         return res
-
-# See serializer/dxfields.py: The portlet fields
-# needs both the portletdataprovider
-# as well as the context. So we extend the
-# default dexterity field serializers.
-
-
-@adapter(IField, IPortletDataProvider, Interface, Interface)
-@implementer(IFieldSerializer)
-class DefaultPortletFieldSerializer(DefaultFieldSerializer):
-
-    def __init__(self, field, portletdata, context, request):
-        self.field = field
-        self.portletdata = portletdata
-        self.context = context
-        self.request = request
-
-    def get_value(self, default=None):
-        return getattr(self.field.interface(self.portletdata),
-                       self.field.__name__,
-                       default)
-
-
-@adapter(IRichText, IPortletDataProvider, Interface, Interface)
-class RichttextPortletFieldSerializer(DefaultPortletFieldSerializer):
-    def __call__(self):
-        value = self.get_value()
-        # self.context is the transform context of the portlet:
-
-        return json_compatible(value, self.context)
-
-
-# TODO: extension of file and image serializers from dxfields.py
-
-
-class NavigationTreeQueryBuilder(NavtreeQueryBuilder):
-    """Build a folder tree query
-
-    Used to build a tab subtree queries for the PortletNavigation class
-    """
-
-    def __init__(self, context, depth):
-        NavtreeQueryBuilder.__init__(self, context)
-        self.query["path"] = {
-            "query": "/".join(context.getPhysicalPath()),
-            "navtree_start": 1,
-            "depth": depth - 1,
-        }
-
-
-def getLocalNavigationRoot(context):
-    return "/".join(context.getPhysicalPath())
-
-
-# nothing is customized here, just this getNavigationRoot method
-class CustomNavtreeStrategy(SitemapNavtreeStrategy):
-
-    def __init__(self, context, bottomLevel=0):
-        SitemapNavtreeStrategy.__init__(self, context, None)
-        self.context = context
-        self.bottomLevel = bottomLevel
-        self.rootPath = self.getRootPath()
-
-    def subtreeFilter(self, node):
-        sitemapDecision = SitemapNavtreeStrategy.subtreeFilter(self, node)
-
-        if sitemapDecision is False:
-            return False
-        depth = node.get("depth", 0)
-
-        if depth > 0 and self.bottomLevel > 0 and depth >= self.bottomLevel:
-            return False
-        else:
-            return True
-
-    def getRootPath(self, topLevel=1):
-        rootPath = getLocalNavigationRoot(self.context)
-
-        rootPath = contextPath = "/".join(self.context.getPhysicalPath())
-
-        if not contextPath.startswith(rootPath):
-            return None
-        contextSubPathElements = contextPath[len(rootPath) + 1:]
-
-        if contextSubPathElements:
-            contextSubPathElements = contextSubPathElements.split("/")
-
-            if len(contextSubPathElements) < topLevel:
-                return None
-            rootPath = (
-                rootPath + "/" + "/".join(contextSubPathElements[:topLevel])
-            )  # noqa
-        else:
-            return None
-
-        return rootPath
 
 
 def get_url(item):
@@ -438,196 +326,135 @@ def get_view_url(context):
     return name, item_url
 
 
-class CatalogNavigationTabs(object):
-    """ Gets the top level children as "tabs"
-    """
-
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
-
-    def _getNavQuery(self, currentFolderOnly):
-                # check whether we only want actions
-        registry = getUtility(IRegistry)
-        navigation_settings = registry.forInterface(
-            INavigationSchema,
-            prefix="plone",
-            check=False
-        )
-        customQuery = getattr(self.context, 'getCustomNavQuery', False)
-
-        if customQuery is not None and utils.safe_callable(customQuery):
-            query = customQuery()
-        else:
-            query = {}
-
-        if currentFolderOnly:
-            path = getLocalNavigationRoot(self.context)
-        else:
-            path = getNavigationRoot(self.context)
-
-        query['path'] = {
-            'query': path,
-            'depth': 1
+class NavtreePortletRenderer(Renderer):
+    def render(self):
+        res = {
+            'title': self.title(),
+            'url': self.heading_link_target(),
+            'has_custom_name': bool(self.hasName()),
+            'items': [],
         }
-        query['portal_type'] = [t for t in navigation_settings.displayed_types]
-        query['sort_on'] = navigation_settings.sort_tabs_on
 
-        if navigation_settings.sort_tabs_reversed:
-            query['sort_order'] = 'reverse'
-        else:
-            query['sort_order'] = 'ascending'
+        if self.include_top:
+            root = self.navigation_root()
 
-        if navigation_settings.filter_on_workflow:
-            query['review_state'] = navigation_settings.workflow_states_to_show
-
-        query['is_default_page'] = False
-
-        if not navigation_settings.nonfolderish_tabs:
-            query['is_folderish'] = True
-
-        return query
-
-    def topLevelTabs(self, currentFolderOnly):
-        context = aq_inner(self.context)
-        mtool = getToolByName(context, 'portal_membership')
-        member = mtool.getAuthenticatedMember().id
-        catalog = getToolByName(context, 'portal_catalog')
-
-        # Build result dict
-        result = []
-
-        query = self._getNavQuery(currentFolderOnly)
-
-        rawresult = catalog.searchResults(query)
-
-        def _get_url(item):
-            if item.getRemoteUrl and not member == item.Creator:
-                return (get_id(item), item.getRemoteUrl)
-
-            return get_view_url(item)
-
-        # now add the content to results
-
-        for item in rawresult:
-            # if item.exclude_from_nav:
-            #     continue
-            cid, item_url = _get_url(item)
-            data = {
-                'name': utils.pretty_title_or_id(context, item),
-                'id': item.getId,
-                'url': item_url,
-                'description': item.Description,
-                'review_state': item.review_state
-            }
-            result.append(data)
-
-        return result
-
-
-class PortletNavigation(object):
-    """ Builds the tree for the navigation portlet serializer
-    """
-
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
-        self.portal = getSite()
-
-    def __call__(self, depth, includeTop, currentFolderOnly):
-        self.depth = depth
-        result = {}
-
-        # uses local context as "root" instead of default INavigationRoot
-        tabs = CatalogNavigationTabs(self.context, self.request)
-        items = []
-
-        for tab in tabs.topLevelTabs(currentFolderOnly):
-            if self.depth != 1:
-                subitems = self.getTabSubTree(
-                    tabUrl=tab["url"], tabPath=tab.get("path")
-                )
-                items.append(
-                    {
-                        "title": tab.get("title", tab.get("name")),
-                        "@id": tab["url"] + "",
-                        "description": tab.get("description", ""),
-                        "items": subitems,
-                    }
-                )
-            else:
-                items.append(
-                    {
-                        "title": tab.get("title", tab.get("name")),
-                        "@id": tab["url"] + "",
-                        "description": tab.get("description", ""),
-                    }
-                )
-
-        if includeTop:
             if utils.safe_hasattr(self.context, 'getRemoteUrl'):
-                item_url = self.context.getRemoteUrl()
+                root_url = root.getRemoteUrl()
             else:
-                cid, item_url = get_view_url(self.context)
-            data = {
-                'title': self.context.pretty_title_or_id(),
-                'id': self.context.getId(),
-                '@id': item_url,
-                'description': self.context.Description(),
-                # 'review_state': content.get_state(self.context)
-            }
-            items = [data] + items
+                cid, root_url = get_view_url(root)
 
-        result["items"] = items
+            root_is_portal = self.root_is_portal()
+            root_title = ('Home'
+                          if root_is_portal else root.pretty_title_or_id())
+            root_type = ('plone-site'
+                         if root_is_portal else utils.normalizeString(
+                             root.portal_type, context=root))
+            normalized_id = utils.normalizeString(root.Title(), context=root)
+            res['items'].append({
+                '@id': root.absolute_url(),
+                'description': root.Description() or '',
+                'href': root_url,
+                'icon': '',
+                'is_current': bool(self.root_item_class()),
+                'is_folderish': True,
+                'is_in_path': True,
+                'items': self.createNavTree(),
+                'normalized_id': normalized_id,
+                'thumb': '',
+                'title': root_title,
+                'type': root_type,
+            })
 
-        return result
+        return res
 
-    def getTabSubTree(self, tabUrl="", tabPath=None):
-        if tabPath is None:
-            # get path for current tab's object
-            tabPath = tabUrl.split(self.portal.absolute_url())[-1]
+    def recurse(self, children, level, bottomLevel):
+        # TODO: we should avoid recursion. This is just a rewrite of the TAL
+        # template, but ideally we should use a dequeue structure to avoid
+        # recursion problems.
 
-            if tabPath == "" or "/view" in tabPath:
-                return ""
+        res = []
 
-            if tabPath.startswith("/"):
-                tabPath = tabPath[1:]
-            elif tabPath.endswith("/"):
-                # we need a real path, without a slash that might appear
-                # at the end of the path occasionally
-                tabPath = str(tabPath.split("/")[0])
+        show_thumbs = not self.data.no_thumbs
+        show_icons = not self.data.no_icons
 
-            if "%20" in tabPath:
-                # we have the space in object's ID that has to be
-                # converted to the real spaces
-                tabPath = tabPath.replace("%20", " ").strip()
-
-        tabObj = self.portal.restrictedTraverse(tabPath, None)
-
-        if tabObj is None:
-            return ""
-
-        strategy = CustomNavtreeStrategy(tabObj, self.depth)
-        queryBuilder = NavigationTreeQueryBuilder(tabObj, self.depth)
-        query = queryBuilder()
-        data = buildFolderTree(
-            tabObj, obj=tabObj, query=query, strategy=strategy)
-
-        return self.recurse(children=data.get("children", []), level=1)
-
-    def recurse(self, children=None, level=0, bottomLevel=0):
-        li = []
+        thumb_scale = self.thumb_scale
+        image_scale = getMultiAdapter((api.portal.get(), self.request),
+                                      name='image_scale')
 
         for node in children:
-            item = {"title": node["Title"], "description": node["Description"]}
-            item["@id"] = node["getURL"]
+            brain = node['item']
 
-            if bottomLevel <= 0 or level <= bottomLevel:
-                nc = node["children"]
-                nc = self.recurse(nc, level + 1, bottomLevel)
+            icon = ''
 
-                if nc:
-                    item["items"] = nc
-            li.append(item)
+            if show_icons:
+                if (node['portal_type'] == 'File'):
+                    icon = self.getMimeTypeIcon(node)
 
-        return li
+            has_thumb = brain.getIcon
+            thumb = ''
+
+            if show_thumbs and has_thumb and thumb_scale:
+                thumb = image_scale.tag(brain, 'image', scale=thumb_scale)
+
+            show_children = node['show_children']
+            item_remote_url = node['getRemoteUrl']
+            use_remote_url = node['useRemoteUrl']
+            item_url = node['getURL']
+            item = {
+                '@id': item_url,
+                'description': node['Description'],
+                'href': item_remote_url if use_remote_url else item_url,
+                'icon': icon,
+                'is_current': node['currentItem'],
+                'is_folderish': node['show_children'],
+                'is_in_path': node['currentParent'],
+                'items': [],
+                'normalized_id': node['normalized_id'],
+                'thumb': thumb,
+                'title': node['Title'],
+                'type': utils.normalizeString(node['portal_type']),
+            }
+
+            if children and show_children and \
+                    ((bottomLevel < level) or (bottomLevel == 0)):
+                children = node['children']
+                item['items'] = self.recurse(
+                    children, level=level + 1, bottomLevel=bottomLevel)
+
+            res.append(item)
+
+        return res
+
+
+# See serializer/dxfields.py: The portlet fields
+# needs both the portletdataprovider
+# as well as the context. So we extend the
+# default dexterity field serializers.
+
+
+@adapter(IField, IPortletDataProvider, Interface, Interface)
+@implementer(IFieldSerializer)
+class DefaultPortletFieldSerializer(DefaultFieldSerializer):
+
+    def __init__(self, field, portletdata, context, request):
+        self.field = field
+        self.portletdata = portletdata
+        self.context = context
+        self.request = request
+
+    def get_value(self, default=None):
+        return getattr(self.field.interface(self.portletdata),
+                       self.field.__name__,
+                       default)
+
+
+@adapter(IRichText, IPortletDataProvider, Interface, Interface)
+class RichttextPortletFieldSerializer(DefaultPortletFieldSerializer):
+    def __call__(self):
+        value = self.get_value()
+        # self.context is the transform context of the portlet:
+
+        return json_compatible(value, self.context)
+
+
+# TODO: extension of file and image serializers from dxfields.py
