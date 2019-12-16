3ea677fc03d64ce375ea79db9d52dc9f5602d910
Katja Suess <k.suess@rohberg.ch>
2019-12-16 10:01:19 +0100

    test if default workflows exist

--- src/plone/restapi/tests/test_services_querystring.py
+++ src/plone/restapi/tests/test_services_querystring.py
@@ -93,9 +93,9 @@ class TestQuerystringEndpoint(unittest.TestCase):
             u"published": {u"title": u"Published with accent \xe9 [published]"},
             u"visible": {u"title": u"Public draft [visible]"},
         }
-        self.assertLessEqual(
-            expected_vocab_values.items(),
-            idx["values"].items())
+        self.assertTrue(
+            all(elem in idx["values"].items() for elem in expected_vocab_values.items())
+        )
 
     def test_endpoint_inlines_operators(self):
         response = self.api_session.get("/@querystring")
