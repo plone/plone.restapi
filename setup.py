277ed3678e4df92db84022c464be1c640d9bb813
Timo Stollenwerk <stollenwerk@kitconcept.com>
2020-02-20 18:03:06 +0100

    Back to development: 6.2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 import sys
 
-version = '6.2.4'
+version = '6.2.5.dev0'
 
 long_description = (
     open("README.rst").read() + "\n" + "Contributors\n"
