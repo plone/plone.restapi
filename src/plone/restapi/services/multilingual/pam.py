d94ce41b5f06dbc535493b01d85aa74c99cdd127
Mikel Larreategi <mlarreategi@codesyntax.com>
2020-03-02 08:33:29 +0100

    remove uneeded stringtype checks

--- src/plone/restapi/services/multilingual/pam.py
+++ src/plone/restapi/services/multilingual/pam.py
@@ -103,23 +103,22 @@ class LinkTranslations(Service):
         return {}
 
     def get_object(self, key):
-        if isinstance(key, six.string_types):
-            if key.startswith(self.portal_url):
-                # Resolve by URL
-                key = key[len(self.portal_url) + 1 :]
-                if six.PY2:
-                    key = key.encode("utf8")
-                return self.portal.restrictedTraverse(key, None)
-            elif key.startswith("/"):
-                if six.PY2:
-                    key = key.encode("utf8")
-                # Resolve by path
-                return self.portal.restrictedTraverse(key.lstrip("/"), None)
-            else:
-                # Resolve by UID
-                brain = self.catalog(UID=key)
-                if brain:
-                    return brain[0].getObject()
+        if key.startswith(self.portal_url):
+            # Resolve by URL
+            key = key[len(self.portal_url) + 1 :]
+            if six.PY2:
+                key = key.encode("utf8")
+            return self.portal.restrictedTraverse(key, None)
+        elif key.startswith("/"):
+            if six.PY2:
+                key = key.encode("utf8")
+            # Resolve by path
+            return self.portal.restrictedTraverse(key.lstrip("/"), None)
+        else:
+            # Resolve by UID
+            brain = self.catalog(UID=key)
+            if brain:
+                return brain[0].getObject()
 
 
 class UnlinkTranslations(Service):
