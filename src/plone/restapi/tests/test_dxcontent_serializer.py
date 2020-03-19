6c0330896ee1d612574ca19767b05f828f584f66
Carsten Senger <senger@rehfisch.de>
2020-03-19 14:32:13 +0100

    fix tests

--- src/plone/restapi/tests/test_dxcontent_serializer.py
+++ src/plone/restapi/tests/test_dxcontent_serializer.py
@@ -4,12 +4,13 @@ from datetime import datetime
 from datetime import time
 from datetime import timedelta
 from DateTime import DateTime
-from plone import api
 from plone.app.discussion.interfaces import IDiscussionSettings
+from plone.app.testing import logout
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.app.textfield.interfaces import ITransformer
 from plone.app.textfield.value import RichTextValue
+from plone.namedfile.file import NamedFile
 from plone.registry.interfaces import IRegistry
 from plone.restapi.interfaces import IExpandableElement
 from plone.restapi.interfaces import ISerializeToJson
@@ -25,7 +26,6 @@ from zope.interface import Interface
 from zope.publisher.interfaces.browser import IBrowserRequest
 
 import json
-import transaction
 import unittest
 
 
@@ -352,16 +352,54 @@ class TestDXContentSerializer(unittest.TestCase):
         self.assertIn("allow_discussion", obj)
         self.assertEqual(False, obj["allow_discussion"])
 
-    def test_primary_field_target(self):
-        file1 = api.content.create(
-            container=self.portal, type="File", id="testfile", title="Testfile"
-        )
-        transaction.commit()
 
-        serializer = getMultiAdapter((file1, self.request), ISerializeToJson)
+class TestDXContentPrimaryFieldTargetUrl(unittest.TestCase):
+
+    layer = PLONE_RESTAPI_DX_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        self.request = self.layer["request"]
+
+        self.portal.invokeFactory(
+            "DXTestDocument",
+            id=u"doc1",
+            test_primary_namedfile_field=NamedFile(
+                data=u"Spam and eggs", contentType=u"text/plain", filename=u"test.txt"
+            ),
+        )
+
+        self.portal.invokeFactory(
+            "DXTestDocument",
+            id=u"doc2",
+            test_primary_namedfile_field=None,
+        )
+
+        self.portal.doc1.creation_date = DateTime("2015-04-27T10:14:48+00:00")
+        self.portal.doc1.modification_date = DateTime("2015-04-27T10:24:11+00:00")
+        IMutableUUID(self.portal.doc1).set("30314724b77a4ec0abbad03d262837aa")
+
+    def serialize(self):
+        serializer = getMultiAdapter((self.portal.doc1, self.request), ISerializeToJson)
+        return serializer()
+
+    def test_primary_field_target(self):
+        logout()
+        serializer = getMultiAdapter((self.portal.doc1, self.request), ISerializeToJson)
         data = serializer()
         self.assertIn("targetUrl", data)
         download_url = u"/".join(
-            [file1.absolute_url(), u"@@download/test_primary_namedfile_field"]
+            [self.portal.doc1.absolute_url(), u"@@download/test_primary_namedfile_field"]
         )
         self.assertEqual(data["targetUrl"], download_url)
+
+    def test_primary_field_target_without_file(self):
+        logout()
+        serializer = getMultiAdapter((self.portal.doc2, self.request), ISerializeToJson)
+        data = serializer()
+        self.assertNotIn("targetUrl", data)
+
+    def test_primary_field_target_with_edit_permissions(self):
+        serializer = getMultiAdapter((self.portal.doc1, self.request), ISerializeToJson)
+        data = serializer()
+        self.assertNotIn("targetUrl", data)
