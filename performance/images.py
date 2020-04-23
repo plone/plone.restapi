5552bbcda1429075eb5edd1a960a3159de7cc8a6
Timo Stollenwerk <stollenwerk@kitconcept.com>
2020-04-23 23:58:45 +0200

    locust: more scales test

--- performance/images.py
+++ performance/images.py
@@ -40,6 +40,19 @@ def image_01mb_get_scale_preview(self):
     )
 
 
+def image_01mb_get_scale_tile(self):
+    headers = {
+        "Accept": "application/json",
+        "Authorization": "Basic YWRtaW46YWRtaW4=",
+        "Content-Type": "application/json",
+    }
+    self.client.get(
+        "/testfolder-read/image-1mb/@@images/image/tile",
+        headers=headers,
+        name="Image 01 MB GET (Scale Tile)",
+    )
+
+
 def image_02mb_get(self):
     headers = {
         "Accept": "application/json",
@@ -79,14 +92,57 @@ def image_10mb_get(self):
     )
 
 
+def image_10mb_get_scale_large(self):
+    headers = {
+        "Accept": "application/json",
+        "Authorization": "Basic YWRtaW46YWRtaW4=",
+        "Content-Type": "application/json",
+    }
+    self.client.get(
+        "/testfolder-read/image-10mb/@@images/image/large",
+        headers=headers,
+        name="Image 10 MB GET (Scale Large)",
+    )
+
+
+def image_10mb_get_scale_preview(self):
+    headers = {
+        "Accept": "application/json",
+        "Authorization": "Basic YWRtaW46YWRtaW4=",
+        "Content-Type": "application/json",
+    }
+    self.client.get(
+        "/testfolder-read/image-10mb/@@images/image/preview",
+        headers=headers,
+        name="Image 10 MB GET (Scale Preview)",
+    )
+
+
+def image_10mb_get_scale_tile(self):
+    headers = {
+        "Accept": "application/json",
+        "Authorization": "Basic YWRtaW46YWRtaW4=",
+        "Content-Type": "application/json",
+    }
+    self.client.get(
+        "/testfolder-read/image-10mb/@@images/image/tile",
+        headers=headers,
+        name="Image 10 MB GET (Scale Tile)",
+    )
+
+
 class UserBehavior(TaskSet):
     tasks = {
-        image_01mb_get: 10,
-        image_01mb_get_scale_large: 20,
-        image_01mb_get_scale_preview: 20,
-        image_02mb_get: 10,
-        image_03mb_get: 10,
-        image_10mb_get: 5,
+        image_01mb_get: 1,
+        image_01mb_get_scale_large: 1,
+        image_01mb_get_scale_preview: 1,
+        image_01mb_get_scale_tile: 1,
+        image_02mb_get: 1,
+        image_03mb_get: 1,
+        image_10mb_get: 1,
+        image_10mb_get_scale_large: 1,
+        image_10mb_get_scale_preview: 1,
+        image_10mb_get_scale_tile: 1,
     }
 
     def on_start(self):
