6c489bb6162e90d02da1261fee1c14648da5268f
Tiberiu Ichim <tiberiuichim@users.noreply.github.com>
2020-05-11 13:48:12 +0300

    Added block transformer concept (#915)
    * Added block transformer concept
    
    * Added WIP image attachment handler
    
    * Removed image block related code
    
    * Added coding magic comment
    
    * Added news changelong entry
    
    * Changed magic code comment
    
    * Changed magic code comment
    
    * Refactored blocks transformer to use deserialization
    
    * Docstring tweak
    
    * Removed IBlockTransformer interface
    
    * Added one generic test for the block deserializer concept
    
    * Fix flake8 problem
    
    * Renamed block deserializer to block converter
    
    * Fix test
    
    * Fix adapter name in configure.zcml
    
    * Rename to IBlockDeserializer; implement transformer as subscription adapters
    
    * Added Image block deserializer
    
    * Added test for multiple deserializer subscribers
    
    * Integrate text deserialization resolveuid method from resolveuid-in-block-more-generic branch
    
    * Better code formatting
    
    * Added test for block_serializer
    
    * Adjust news entry
    
    * Rollback .gitignore
    
    * Use current plone.restapi master version of path2uid
    
    * Added test for block image deserializer
    
    * Renamed interfaces to IBlockFieldSerializationTransformer and IBlockFieldDeserializationTransformer
    
    * Added block_type attribute in ifaces declaration
    
    * Run black on src/

--- src/plone/restapi/serializer/blocks.py
+++ src/plone/restapi/serializer/blocks.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from plone.outputfilters.browser.resolveuid import uuidToURL
 from plone.restapi.behaviors import IBlocks
+from plone.restapi.interfaces import IBlockFieldSerializationTransformer
 from plone.restapi.interfaces import IFieldSerializer
 from plone.restapi.serializer.converters import json_compatible
 from plone.restapi.serializer.dxfields import DefaultFieldSerializer
 from plone.schema import IJSONField
 from zope.component import adapter
+from zope.component import subscribers
 from zope.interface import implementer
 from zope.interface import Interface
+from zope.publisher.interfaces.browser import IBrowserRequest
 
 import copy
 import re
@@ -22,25 +25,55 @@ class BlocksJSONFieldSerializer(DefaultFieldSerializer):
     def __call__(self):
         value = copy.deepcopy(self.get_value())
 
-        # Resolve UID links
         if self.field.getName() == "blocks":
-            for block in value.values():
-                if block.get("@type") == "text":
-                    entity_map = block.get("text", {}).get("entityMap", {})
-                    for entity in entity_map.values():
-                        if entity.get("type") == "LINK":
-                            href = entity.get("data", {}).get("url", "")
-                            before = href  # noqa
-                            if href:
-                                match = RESOLVEUID_RE.match(href)
-                                if match is not None:
-                                    uid, suffix = match.groups()
-                                    href = uuidToURL(uid)
-                                    if href is None:
-                                        continue
-                                    if suffix:
-                                        href += "/" + suffix
-                                    entity["data"]["href"] = href
-                                    entity["data"]["url"] = href
-                                    print("SERIALIZE " + before + " -> " + href)  # noqa
+            for id, block_value in value.items():
+                block_type = block_value.get("@type", "")
+
+                handlers = [
+                    h
+                    for h in subscribers(
+                        (self.context, self.request),
+                        IBlockFieldSerializationTransformer,
+                    )
+                    if h.block_type == block_type
+                ]
+
+                for handler in sorted(handlers, key=lambda h: h.order):
+                    block_value = handler(block_value)
+
+                value[id] = block_value
+
         return json_compatible(value)
+
+
+@implementer(IBlockFieldSerializationTransformer)
+@adapter(IBlocks, IBrowserRequest)
+class TextBlockSerializer(object):
+    order = 100
+    block_type = "text"
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self, value):
+        # Resolve UID links
+        entity_map = value.get("text", {}).get("entityMap", {})
+        for entity in entity_map.values():
+            if entity.get("type") == "LINK":
+                href = entity.get("data", {}).get("url", "")
+                before = href  # noqa
+                if href:
+                    match = RESOLVEUID_RE.match(href)
+                    if match is not None:
+                        uid, suffix = match.groups()
+                        href = uuidToURL(uid)
+                        if href is None:
+                            continue
+                        if suffix:
+                            href += "/" + suffix
+                        entity["data"]["href"] = href
+                        entity["data"]["url"] = href
+                        print("SERIALIZE " + before + " -> " + href)  # noqa
+
+        return value
