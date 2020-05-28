13bb2363e34fc756e80b44ece9508b42eb8d9350
Alin Voinea <contact@avoinea.com>
2020-05-28 13:18:00 +0300

    Cosmetics

--- src/plone/restapi/services/types/add.py
+++ src/plone/restapi/services/types/add.py
@@ -27,73 +27,74 @@ class TypesPost(Service):
         return self
 
     def reply(self):
-        if self.params and len(self.params) > 0:
+        if not self.params:
+            raise BadRequest("Missing parameter typename")
 
-            data = json_body(self.request)
+        data = json_body(self.request)
 
-            title = data.get("title", None)
-            if not title:
-                raise BadRequest("Property 'title' is required")
+        title = data.get("title", None)
+        if not title:
+            raise BadRequest("Property 'title' is required")
 
-            tid = data.get("id", None)
-            if not tid:
-                tid = idnormalizer.normalize(title).replace("-", "_")
+        tid = data.get("id", None)
+        if not tid:
+            tid = idnormalizer.normalize(title).replace("-", "_")
 
-            description = data.get("description", "")
+        description = data.get("description", "")
 
-            # Disable CSRF protection
-            if "IDisableCSRFProtection" in dir(plone.protect.interfaces):
-                alsoProvides(
-                    self.request,
-                    plone.protect.interfaces.IDisableCSRFProtection
-                )
-
-            # Make sure we don't get the right dexterity-types adapter
-            if IPloneRestapiLayer.providedBy(self.request):
-                noLongerProvides(self.request, IPloneRestapiLayer)
-
-            name = self.params.pop()
-            context = queryMultiAdapter(
-                (self.context, self.request), name="dexterity-types"
+        # Disable CSRF protection
+        if "IDisableCSRFProtection" in dir(plone.protect.interfaces):
+            alsoProvides(
+                self.request,
+                plone.protect.interfaces.IDisableCSRFProtection
             )
-            context = context.publishTraverse(self.request, name)
 
-            factory = data.get('@type', '')
-            if not factory:
-                raise BadRequest("Property '@type' is required")
+        # Make sure we get the right dexterity-types adapter
+        if IPloneRestapiLayer.providedBy(self.request):
+            noLongerProvides(self.request, IPloneRestapiLayer)
 
-            if factory == "fieldset":
-                # Adding new fieldset
-                add = queryMultiAdapter((context, self.request),
-                                        name="add-fieldset")
-                properties = {
-                    "label": title,
-                    "__name__": tid,
-                    "description": description
-                }
-            else:
-                klass = None
-                vocabulary = queryUtility(IVocabularyFactory, name='Fields')
-                for term in vocabulary(context):
-                    if factory in (term.title, term.token):
-                        klass = term.value
+        name = self.params.pop()
+        context = queryMultiAdapter(
+            (self.context, self.request), name="dexterity-types"
+        )
+        context = context.publishTraverse(self.request, name)
 
-                if not klass:
-                    raise BadRequest("Invalid '@type' %s" % factory)
+        factory = data.get('@type', '')
+        if not factory:
+            raise BadRequest("Property '@type' is required")
 
-                # Adding new field
-                add = queryMultiAdapter((context, self.request),
-                                        name="add-field")
-                properties = {
-                    "title": title,
-                    "__name__": tid,
-                    "description": description,
-                    "factory": klass,
-                    "required": data.get("required", False)
-                }
+        # Adding new fieldset
+        if factory == "fieldset":
+            add = queryMultiAdapter((context, self.request),
+                                    name="add-fieldset")
+            properties = {
+                "label": title,
+                "__name__": tid,
+                "description": description
+            }
+        # Adding new field
+        else:
+            klass = None
+            vocabulary = queryUtility(IVocabularyFactory, name='Fields')
+            for term in vocabulary(context):
+                if factory in (term.title, term.token):
+                    klass = term.value
 
-            field = add.form_instance.create(data=properties)
-            add.form_instance.add(field)
+            if not klass:
+                raise BadRequest("Invalid '@type' %s" % factory)
+
+            add = queryMultiAdapter((context, self.request),
+                                    name="add-field")
+            properties = {
+                "title": title,
+                "__name__": tid,
+                "description": description,
+                "factory": klass,
+                "required": data.get("required", False)
+            }
+
+        field = add.form_instance.create(data=properties)
+        add.form_instance.add(field)
 
         # TODO Return added field/fieldset
         return self.reply_no_content()
