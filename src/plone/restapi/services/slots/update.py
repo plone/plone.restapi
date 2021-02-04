0302833923990e4751de81c8b83eaf2c2bfb12fa
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-02-04 18:25:05 +0200

    Improve patch service

--- src/plone/restapi/services/slots/update.py
+++ src/plone/restapi/services/slots/update.py
@@ -6,9 +6,10 @@
 from plone.restapi.exceptions import DeserializationError
 from plone.restapi.interfaces import IDeserializeFromJson
 from plone.restapi.interfaces import ISerializeToJson
+from plone.restapi.interfaces import ISlots
 from plone.restapi.services import Service
 from plone.restapi.services.locking.locking import is_locked
-from zope.component import queryMultiAdapter
+from zope.component import getMultiAdapter
 
 
 class SlotsPatch(Service):
@@ -23,25 +24,18 @@ class SlotsPatch(Service):
         return self
 
     def reply(self):
-        if self.params and len(self.params) > 0:
-            return self.replySlot()
-
         if is_locked(self.context, self.request):
             self.request.response.setStatus(403)
             return dict(error=dict(type="Forbidden", message="Resource is locked."))
 
-        deserializer = queryMultiAdapter(
-            (self.context, self.request), IDeserializeFromJson
+        if self.params and len(self.params) > 0:
+            return self.replySlot()
+
+        storage = ISlots(self.context)
+
+        deserializer = getMultiAdapter(
+            (self.context, storage, self.request), IDeserializeFromJson
         )
-        if deserializer is None:
-            self.request.response.setStatus(501)
-            return dict(
-                error=dict(
-                    message="Cannot deserialize type {}".format(
-                        self.context.portal_type
-                    )
-                )
-            )
 
         try:
             deserializer()
@@ -53,11 +47,38 @@ class SlotsPatch(Service):
         if prefer == "return=representation":
             self.request.response.setStatus(200)
 
-            serializer = queryMultiAdapter(
-                (self.context, self.request), ISerializeToJson
+            serializer = getMultiAdapter(
+                (self.context, storage, self.request), ISerializeToJson
             )
 
             serialized_obj = serializer()
+            return serialized_obj
+
+        return self.reply_no_content()
+
+    def replySlot(self):
+        name = self.params[0]
+        storage = ISlots(self.context)
+        slot = storage[name]
+
+        deserializer = getMultiAdapter(
+            (self.context, slot, self.request), IDeserializeFromJson
+        )
+        try:
+            deserializer()
+        except DeserializationError as e:
+            self.request.response.setStatus(400)
+            return dict(error=dict(type="DeserializationError", message=str(e)))
+
+        prefer = self.request.getHeader("Prefer")
+        if prefer == "return=representation":
+            self.request.response.setStatus(200)
+
+            serializer = getMultiAdapter(
+                (self.context, slot, self.request), ISerializeToJson
+            )
+
+            serialized_obj = serializer(name)
             return serialized_obj
 
         return self.reply_no_content()
