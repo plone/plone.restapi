62676e4e54e8119b5782b503793651d7059d514d
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-02-09 07:46:19 +0200

    Shuffle events code

---
+++ src/plone/restapi/events.py
@@ -0,0 +1,18 @@
+from plone.restapi.interfaces import IBlockRemovedEvent
+from plone.restapi.interfaces import IBlocksRemovedEvent
+from zope.interface import implements
+from zope.interface.interfaces import ObjectEvent
+
+
+class BlockRemovedEvent(ObjectEvent):
+    implements(IBlockRemovedEvent)
+
+    def __init__(self, object):
+        self.object = object
+
+
+class BlocksRemovedEvent(ObjectEvent):
+    implements(IBlocksRemovedEvent)
+
+    def __init__(self, object):
+        self.object = object
--- src/plone/restapi/slots/handlers.py
+++ src/plone/restapi/slots/handlers.py
@@ -1,28 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from plone.api import portal
-from plone.restapi.interfaces import IBlockRemovedEvent
-from plone.restapi.interfaces import IBlocksRemovedEvent
 from plone.restapi.interfaces import ISlots
-from zope.interface import implements
-from zope.interface.interfaces import ObjectEvent
-
-
-class BlockRemovedEvent(ObjectEvent):
-    implements(IBlockRemovedEvent)
-
-    def __init__(self, object):
-        self.object = object
-
-
-class BlocksRemovedEvent(ObjectEvent):
-    implements(IBlocksRemovedEvent)
-
-    def __init__(self, object):
-        self.object = object
 
 
 def handle_block_removed_event(event):
+    # TODO: needs rewrite
     info = event.object
     catalog = portal.get_tool('portal_catalog')
     blockid = info['blockid']
@@ -47,6 +30,7 @@ def handle_blocks_removed_event(event):
     """ Update the slot blocks layout for objects that reference some blocks
     """
 
+    # TODO: needs rewrite
     info = event.object
     catalog = portal.get_tool('portal_catalog')
     blockids = info['blocks'].keys()
