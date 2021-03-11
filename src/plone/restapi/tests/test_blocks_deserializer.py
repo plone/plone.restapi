8899017db6eacf91c07ccb06b8e43244cb6613e6
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-03-11 13:28:40 +0200

    Add subscriber unsubscription

--- src/plone/restapi/tests/test_blocks_deserializer.py
+++ src/plone/restapi/tests/test_blocks_deserializer.py
@@ -8,6 +8,7 @@ from plone.restapi.interfaces import IDeserializeFromJson
 from plone.restapi.testing import PLONE_RESTAPI_DX_INTEGRATION_TESTING
 from plone.uuid.interfaces import IUUID
 from zope.component import adapter
+from zope.component import getGlobalSiteManager
 from zope.component import getMultiAdapter
 from zope.component import provideSubscriptionAdapter
 from zope.component import queryUtility
@@ -76,6 +77,13 @@ class TestBlocksDeserializer(unittest.TestCase):
         assert self.portal.doc1._handler_called is True
         assert self.portal.doc1.blocks["123"]["value"] == u"changed: text"
 
+        sm = getGlobalSiteManager()
+        sm.adapters.unsubscribe(
+            (IDexterityItem, IBrowserRequest),
+            IBlockFieldDeserializationTransformer,
+            TestAdapter,
+        )
+
     def test_disabled_deserializer(self):
         @implementer(IBlockFieldDeserializationTransformer)
         @adapter(IBlocks, IBrowserRequest)
@@ -104,6 +112,13 @@ class TestBlocksDeserializer(unittest.TestCase):
 
         assert not getattr(self.portal.doc1, "_handler_called", False)
         assert self.portal.doc1.blocks["123"]["value"] == u"text"
+
+        sm = getGlobalSiteManager()
+        sm.adapters.unsubscribe(
+            (IDexterityItem, IBrowserRequest),
+            IBlockFieldDeserializationTransformer,
+            TestAdapter,
+        )
 
     def test_register_multiple_transform(self):
         @implementer(IBlockFieldDeserializationTransformer)
@@ -155,6 +170,18 @@ class TestBlocksDeserializer(unittest.TestCase):
         self.assertTrue(self.portal.doc1._handler_called_a)
         self.assertTrue(self.portal.doc1._handler_called_b)
         self.assertEqual(self.portal.doc1.blocks["123"]["value"], u"c")
+
+        sm = getGlobalSiteManager()
+        sm.adapters.unsubscribe(
+            (IDexterityItem, IBrowserRequest),
+            IBlockFieldDeserializationTransformer,
+            TestAdapterA,
+        )
+        sm.adapters.unsubscribe(
+            (IDexterityItem, IBrowserRequest),
+            IBlockFieldDeserializationTransformer,
+            TestAdapterB,
+        )
 
     def test_blocks_html_cleanup(self):
         self.deserialize(
