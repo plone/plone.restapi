35203c77bfcd72e677bdd19fb4a615baddde1576
Timo Stollenwerk <stollenwerk@kitconcept.com>
2021-06-04 09:40:25 +0200

    flake8

--- src/plone/restapi/testing.py
+++ src/plone/restapi/testing.py
@@ -238,9 +238,7 @@ class PloneRestApiDXIterateLayer(PloneSandboxLayer):
     def setUpZope(self, app, configurationContext):
         import plone.restapi
 
-        xmlconfig.file(
-            "configure.zcml", plone.restapi, context=configurationContext
-        )
+        xmlconfig.file("configure.zcml", plone.restapi, context=configurationContext)
         xmlconfig.file("testing.zcml", plone.restapi, context=configurationContext)
 
         z2.installProduct(app, "plone.restapi")
