cfae76cb8247d066a6c85ee941c195cdd9b560fa
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-07-05 16:15:45 +0300

    Add info about attributes

--- src/plone/restapi/slots/interfaces.py
+++ src/plone/restapi/slots/interfaces.py
@@ -27,3 +27,18 @@ class ISlotSettings(Interface):
         description=u'Editable slots using "Modify portal content" permission',
         value_type=TextLine(title=u"Slot name"),
     )
+
+
+"""
+# special slot fill attributes:
+
+- `s:sameAs`: Ideally blocks uids should be unique across the whole CMS. That's why we
+  use uuid, right? So, when customizing the order of an inherited slot, a "placeholder"
+  should be created in the current level. This placeholder would have `s:sameAs` point to
+  the "overriden" parent.
+- `s:isVariantOf`: An inherited block could be "copied" to another level and mutated
+  there. `s:isVariantOf` points to the original source block.
+- `v:hidden`: An inherited block is hidden at this level. To be used with `s:sameAs` to
+  point to the "parent" block. When publishing slots (in the `@slots` endpoint) the
+  hidden blocks are included only when run with `full=true`.
+"""
