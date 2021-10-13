083a90a41cb94ce434ac8bb89dd81635dca331ef
Victor Fernandez de Alba <sneridagh@gmail.com>
2021-10-13 19:55:32 +0200

    Fix tests

--- src/plone/restapi/tests/test_batching.py
+++ src/plone/restapi/tests/test_batching.py
@@ -281,7 +281,7 @@ class TestBatchingSiteRoot(TestBatchingDXBase):
         response = self.api_session.get("/?b_start=2&b_size=2")
 
         # Response should contain canonical URL without batching params
-        self.assertEqual(response.json()["@id"], self.portal_url + "/")
+        self.assertEqual(response.json()["@id"], self.portal_url)
 
     def test_contains_batching_links(self):
         # Fetch the second page of the batch
