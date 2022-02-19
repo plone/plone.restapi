44146f858ec4481e2a9a31991f2c7b9c3c8c6b03
Timo Stollenwerk <stollenwerk@kitconcept.com>
2022-02-19 07:17:55 +0100

    Accept start/end params

--- src/plone/restapi/services/transactions/get.py
+++ src/plone/restapi/services/transactions/get.py
@@ -4,8 +4,32 @@ from plone.restapi.services import Service
 
 class TransactionsGet(Service):
     def reply(self):
+        # get parameters
+        start = self.request.form.get("start", 0)
+        end = self.request.form.get("end", 100)
+        try:
+            start = int(start)
+        except ValueError as e:
+            self.request.response.setStatus(422)
+            return dict(
+                error=dict(
+                    type="Invalid parameter 'start'",
+                    message=e.args[0],
+                )
+            )
+        try:
+            end = int(end)
+        except ValueError as e:
+            self.request.response.setStatus(422)
+            return dict(
+                error=dict(
+                    type="Invalid parameter 'end'",
+                    message=e.args[0],
+                )
+            )
+
         db = self.context._p_jar.db()
-        return json_compatible(db.undoLog(0, 100))
+        return json_compatible(db.undoLog(start, end))
         return db.undoLog(0, 100, filter=(lambda user_name: "Plone _perftest"))
 
         # return {
