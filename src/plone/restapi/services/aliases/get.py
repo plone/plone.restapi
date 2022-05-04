ae1b4f94aa810e74f6f1017b7e4908293520df84
Petchesi-Iulian <iamnotacat12@gmail.com>
2022-05-04 18:02:32 +0300

    Remove portal root from url

--- src/plone/restapi/services/aliases/get.py
+++ src/plone/restapi/services/aliases/get.py
@@ -3,6 +3,7 @@ from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
 from zope.component import getUtility
 from plone.app.redirector.interfaces import IRedirectionStorage
+from zope.component.hooks import getSite
 
 
 @implementer(IPublishTraverse)
@@ -11,5 +12,13 @@ class AliasesGet(Service):
         storage = getUtility(IRedirectionStorage)
         context_path = "/".join(self.context.getPhysicalPath())
         redirects = storage.redirects(context_path)
+        aliases = [deroot_path(alias) for alias in redirects]
         self.request.response.setStatus(201)
-        return {"aliases": redirects}
+        return {"aliases": aliases}
+
+
+def deroot_path(path):
+    """ Remove the portal root from alias """
+    portal = getSite()
+    root_path = "/".join(portal.getPhysicalPath())
+    return path.replace(root_path, "", 1)
