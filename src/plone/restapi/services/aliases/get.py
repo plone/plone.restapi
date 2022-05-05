c5a91ca76ca8ad4c316ba4072f72236b75aa03a9
Petchesi-Iulian <iamnotacat12@gmail.com>
2022-05-05 16:02:00 +0300

    Added GET root endpoint.

--- src/plone/restapi/services/aliases/get.py
+++ src/plone/restapi/services/aliases/get.py
@@ -4,6 +4,8 @@ from zope.publisher.interfaces import IPublishTraverse
 from zope.component import getUtility
 from plone.app.redirector.interfaces import IRedirectionStorage
 from zope.component.hooks import getSite
+from Products.CMFPlone.controlpanel.browser.redirects import RedirectsControlPanel
+from plone.restapi.serializer.converters import datetimelike_to_iso
 
 
 @implementer(IPublishTraverse)
@@ -20,12 +22,19 @@ class AliasesGet(Service):
 @implementer(IPublishTraverse)
 class AliasesRootGet(Service):
     def reply(self):
-        storage = getUtility(IRedirectionStorage)
-        context_path = "/".join(self.context.getPhysicalPath())
-        redirects = storage.redirects(context_path)
-        aliases = [deroot_path(alias) for alias in redirects]
+        """
+            redirect-to - target
+            path        - path
+            redirect    - full path with root
+        """
+        batch = RedirectsControlPanel(self.context, self.request).redirects()
+        redirects = [entry for entry in batch]
+
+        for redirect in redirects:
+            redirect['datetime'] = datetimelike_to_iso(redirect['datetime'])
         self.request.response.setStatus(201)
-        return {"aliases": aliases}
+
+        return {"aliases": redirects}
 
 
 def deroot_path(path):
