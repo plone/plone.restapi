4023828e1d308a864b45039599a2efab6ebd3fd6
Petchesi Iulian <iamnotacat12@gmail.com>
2022-05-06 13:30:40 +0300

    @aliases Root endpoints (#1406)
    * Initial commit.
    
    * Added GET root endpoint.
    
    * Added @aliases root POST
    
    * Refs #1393 - Add @aliases endpoint docs
    
    * Flake8 & delete endpoint
    
    * Black
    
    Co-authored-by: Alin Voinea <contact@avoinea.com>

--- src/plone/restapi/services/aliases/get.py
+++ src/plone/restapi/services/aliases/get.py
@@ -4,6 +4,8 @@ from zope.publisher.interfaces import IPublishTraverse
 from zope.component import getUtility
 from plone.app.redirector.interfaces import IRedirectionStorage
 from zope.component.hooks import getSite
+from Products.CMFPlone.controlpanel.browser.redirects import RedirectsControlPanel
+from plone.restapi.serializer.converters import datetimelike_to_iso
 
 
 @implementer(IPublishTraverse)
@@ -15,6 +17,24 @@ class AliasesGet(Service):
         aliases = [deroot_path(alias) for alias in redirects]
         self.request.response.setStatus(201)
         return {"aliases": aliases}
+
+
+@implementer(IPublishTraverse)
+class AliasesRootGet(Service):
+    def reply(self):
+        """
+        redirect-to - target
+        path        - path
+        redirect    - full path with root
+        """
+        batch = RedirectsControlPanel(self.context, self.request).redirects()
+        redirects = [entry for entry in batch]
+
+        for redirect in redirects:
+            redirect["datetime"] = datetimelike_to_iso(redirect["datetime"])
+        self.request.response.setStatus(201)
+
+        return {"aliases": redirects}
 
 
 def deroot_path(path):
