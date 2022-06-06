1fdb73eac281e48181a04b2e7c4fe79d31d25aeb
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2022-06-06 17:51:47 +0300

    return assignable_rules on get

--- src/plone/restapi/services/rules/get.py
+++ src/plone/restapi/services/rules/get.py
@@ -2,17 +2,19 @@ from plone.app.contentrules.browser.assignments import ManageAssignments
 from plone.restapi.services import Service
 
 
-class RulesGet(Service):
-    """Publishes the rules assigned or acquired by given context"""
+class ContentRulesGet(Service):
+    """Publishes the content rules assigned or acquired by given context"""
 
     def reply(self):
         manage_assignments = ManageAssignments(self.context, self.request)
         acquired_rules = manage_assignments.acquired_rules()
         assigned_rules = manage_assignments.assigned_rules()
+        assignable_rules = manage_assignments.assignable_rules()
 
         return {
-            "rules": {
+            "content-rules": {
                 "acquired_rules": acquired_rules,
                 "assigned_rules": assigned_rules,
+                "assignable_rules": assignable_rules,
             }
         }
