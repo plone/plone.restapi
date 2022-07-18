660d514bda4620693d9e314231cb8ce95dce5aa5
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2022-07-18 20:12:08 +0300

    add and delete conditions and rules

--- src/plone/restapi/controlpanels/rules.py
+++ src/plone/restapi/controlpanels/rules.py
@@ -1,15 +1,17 @@
+from zExceptions import BadRequest
 from zope.component import adapter
 from zope.component import queryMultiAdapter
 from zope.interface import alsoProvides
 from zope.interface import implementer
 from zope.interface import Interface
-from plone.restapi.interfaces import ISerializeToJson
+from zope.publisher.interfaces.browser import IBrowserPublisher
+from z3c.form import interfaces
+from plone.restapi.interfaces import ISerializeToJson, IDeserializeFromJson
+from plone.restapi.interfaces import IPloneRestapiLayer
 from plone.restapi.controlpanels import RegistryConfigletPanel
 from plone.restapi.controlpanels.interfaces import IContentRulesControlpanel
-from zope.publisher.interfaces.browser import IBrowserPublisher
-from zope.interface import noLongerProvides
+from plone.restapi.deserializer import json_body
 import plone.protect.interfaces
-from plone.restapi.interfaces import IPloneRestapiLayer
 
 
 @adapter(Interface, IPloneRestapiLayer)
@@ -23,35 +25,40 @@ class ContentRulesControlpanel(RegistryConfigletPanel):
 
         return self.context.restrictedTraverse("++rule++" + name)
 
-    # def add(self, names):
-    #     data = json_body(self.request)
-
-    #     title = data.get("title", None)
-    #     if not title:
-    #         raise BadRequest("Property 'title' is required")
-
-    #     tid = data.get("id", None)
-    #     if not tid:
-    #         tid = idnormalizer.normalize(title).replace("-", "_")
-
-    #     description = data.get("description", "")
-
-    #     properties = {"id": tid, "title": title, "description": description}
-
-    #     # Disable CSRF protection
-    #     if "IDisableCSRFProtection" in dir(plone.protect.interfaces):
-    #         alsoProvides(self.request, plone.protect.interfaces.IDisableCSRFProtection)
-
-    #     #if IPloneRestapiLayer.providedBy(self.request):
-    #     #    noLongerProvides(self.request, IPloneRestapiLayer)
-
-    #     context = queryMultiAdapter(
-    #         (self.context, self.request), name="content-rules"
-    #     )
-    #     add_type = queryMultiAdapter((context, self.request), name="add-type")
-    #     fti = add_type.form_instance.create(data=properties)
-    #     add_type.form_instance.add(fti)
-    #     return self.get([tid])
+    def add(self, names):
+        data = json_body(self.request)
+        rules = queryMultiAdapter((self.context, self.request), name="+rule")
+        if len(names) == 1:
+            view = queryMultiAdapter((rules, self.request), name="plone.ContentRule")
+            form = view.form_instance
+            form.update()
+            title = data.get("title", None)
+            if not title:
+                raise BadRequest("Property 'title' is required")
+            widget = form.widgets["event"]
+            data["event"] = interfaces.IDataConverter(widget).toFieldValue([data["event"]])
+            if "IDisableCSRFProtection" in dir(plone.protect.interfaces):
+                alsoProvides(self.request, plone.protect.interfaces.IDisableCSRFProtection)
+            rule = form.create(data)
+            form.add(rule)
+        else:
+            # we need to add a condition or action to the current rule
+            if len(names) == 2:
+                raise BadRequest("%s type is required" % extra.title())
+            extra =  names[1]
+            rule = self.publishTraverse(self.request, name=names[0])
+            view_name = names[2]
+            extra_ob = self.context.restrictedTraverse(
+                "++rule++" + names[0] + "/+%s" % extra
+            )
+            view = queryMultiAdapter((extra_ob, self.request), name=view_name)
+            form = view.form_instance
+            form.update()
+            if "IDisableCSRFProtection" in dir(plone.protect.interfaces):
+                alsoProvides(self.request, plone.protect.interfaces.IDisableCSRFProtection)
+            extra_ob = form.create(data)
+            form.add(extra_ob)
+        return self.get([rule.__name__])
 
     def get(self, names):
         name = names[0]
@@ -75,15 +82,24 @@ class ContentRulesControlpanel(RegistryConfigletPanel):
     #     return deserializer(context)
 
     def delete(self, names):
-        name = names[0]
-        self.request['rule-id'] = name
+        if len(names) == 1:
+            name = names[0]
+            self.request["rule-id"] = name
 
-        cpanel = queryMultiAdapter(
-            (self.context, self.request), name="rules-controlpanel"
-        )
-        # Disable CSRF protection
-        # The "regular" way to force authorization was via the interface
-        # IDisableCSRFProtection, but the plone.app.contentrules controlpanel
-        # calls authorize directly, so we need to override that here
-        cpanel.authorize = lambda: True
-        cpanel.delete_rule()
+            cpanel = queryMultiAdapter(
+                (self.context, self.request), name="rules-controlpanel"
+            )
+            # Disable CSRF protection
+            # The "regular" way to force authorization was via the interface
+            # IDisableCSRFProtection, but the plone.app.contentrules controlpanel
+            # calls authorize directly, so we need to override that here
+            cpanel.authorize = lambda: True
+            cpanel.delete_rule()
+        else:
+            # we need to delete a condition or action from the current rule
+            if len(names) == 2:
+                raise BadRequest("%s index is required" % extra.title())
+            extra =  names[1]
+            rule = self.publishTraverse(self.request, name=names[0])
+            extras = getattr(rule, extra)
+            del extras[int(names[2])]
