c91779bd4713ddc066afd80a1b35657b47978d5b
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-07-29 13:45:42 +0200

    @site and @navroot endpoint implementations

---
+++ src/plone/restapi/services/navroot/get.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from plone.restapi.services import Service
+from zope.component import getMultiAdapter
+
+
+class NavrootGet(Service):
+    def reply(self):
+        portal_state = getMultiAdapter(
+            (self.context, self.request), name="plone_portal_state"
+        )
+        return {
+            "@id": portal_state.navigation_root().absolute_url(),
+            "title": portal_state.navigation_root_title(),
+        }
---
+++ src/plone/restapi/services/site/get.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from plone.restapi.services import Service
+from zope.component import getMultiAdapter
+
+
+class SiteGet(Service):
+    def reply(self):
+        portal_state = getMultiAdapter(
+            (self.context, self.request), name="plone_portal_state"
+        )
+        return {
+            "title": portal_state.portal_title,
+        }
