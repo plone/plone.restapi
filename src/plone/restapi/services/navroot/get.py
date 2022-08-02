c6b4b9c7bad81903ec075d5a4f3ee7feb95749cf
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-08-02 10:45:07 +0200

    Additional items in site endpoint

--- src/plone/restapi/services/navroot/get.py
+++ src/plone/restapi/services/navroot/get.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+from plone.restapi.interfaces import IExpandableElement
 from plone.restapi.services import Service
+from zope.component import adapter
 from zope.component import getMultiAdapter
+from zope.interface import implementer
+from zope.interface import Interface
 
 
 class NavrootGet(Service):
@@ -8,7 +12,18 @@ class NavrootGet(Service):
         portal_state = getMultiAdapter(
             (self.context, self.request), name="plone_portal_state"
         )
-        return {
-            "@id": portal_state.navigation_root_url(),
-            "title": portal_state.navigation_root_title(),
-        }
+        result = {}
+        result["navroot"].update(
+            {
+                "url": portal_state.navigation_root_url(),
+                "title": portal_state.navigation_root_title(),
+            }
+        )
+
+        return result
+
+
+class NavrootGet(Service):
+    def reply(self):
+        navroot = NavrootGet(self.context, self.request)
+        return navroot(expand=True)["navroot"]
--- src/plone/restapi/services/site/get.py
+++ src/plone/restapi/services/site/get.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+from plone.registry.interfaces import IRegistry
 from plone.restapi.services import Service
+from Products.CMFPlone.interfaces import IImagingSchema
+from Products.CMFPlone.interfaces import ISiteSchema
+from Products.CMFPlone.utils import getSiteLogo
 from zope.component import getMultiAdapter
+from zope.component import getUtility
 
 
 class SiteGet(Service):
@@ -8,6 +13,16 @@ class SiteGet(Service):
         portal_state = getMultiAdapter(
             (self.context, self.request), name="plone_portal_state"
         )
+        registry = getUtility(IRegistry)
+        site_settings = registry.forInterface(
+            ISiteSchema, prefix="plone", check=False
+        )
+        image_settings = registry.forInterface(
+            IImagingSchema, prefix="plone", check=False
+        )
         return {
             "title": portal_state.portal_title(),
+            "logo": site_settings.site_logo and getSiteLogo() or None,
+            "robots_txt": site_settings.robots_txt,
+            "imaging_allowed_sizes": image_settings.allowed_sizes,
         }
