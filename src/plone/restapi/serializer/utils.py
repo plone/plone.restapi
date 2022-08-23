d35126751afcc5d3c5ea222af571dcb430c20f38
Anirudh H Kashyap <68481927+anirudhhkashyap@users.noreply.github.com>
2022-08-23 23:00:45 +0530

    Add unrestricted keyword argument to uuidToObject() (#1471)
    * Add unrestricted keyword argument to uuidToObject()
    
    Setting the unrestricted flag to True invokes the unrestrictedTraversal() instead of restrictedTraversal().
    Invoking the latter threw the Unauthorized Exception. Now the object can be accessed depending on the unrestricted flag.
    
    * Update utils.py
    
    Made changes suggested in code review.

--- src/plone/restapi/serializer/utils.py
+++ src/plone/restapi/serializer/utils.py
@@ -1,4 +1,4 @@
-from plone.outputfilters.browser.resolveuid import uuidToObject
+from plone.app.uuid.utils import uuidToObject
 from plone.outputfilters.browser.resolveuid import uuidToURL
 from plone.restapi.interfaces import IObjectPrimaryFieldTarget
 from zope.component import queryMultiAdapter
@@ -8,7 +8,7 @@ import re
 
 RESOLVEUID_RE = re.compile("^[./]*resolve[Uu]id/([^/]*)/?(.*)$")
 
-
+# Takes the resolveID URL and returns a URL to the actual object
 def uid_to_url(path):
     if not path:
         return ""
@@ -23,7 +23,10 @@ def uid_to_url(path):
     if suffix:
         href += "/" + suffix
     else:
-        target_object = uuidToObject(uid)
+        # Pass unrestricted flag as true so the object is accessible.
+        # At uuidToObject(), this leads to unrestrictedTraverse() to be invoked instead of restrictedTraverse().
+        
+        target_object = uuidToObject(uid, unrestricted=True)
         if target_object:
             adapter = queryMultiAdapter(
                 (target_object, target_object.REQUEST), IObjectPrimaryFieldTarget
