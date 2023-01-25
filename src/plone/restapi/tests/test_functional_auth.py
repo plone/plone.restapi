3fd0911574d9ac348ccf8f1bc8bf821a8e322b6b
David Glick <david@glicksoftware.com>
2023-01-25 11:46:52 -0800

    Fix auth cookie for Zope users in login endpoint

--- src/plone/restapi/tests/test_functional_auth.py
+++ src/plone/restapi/tests/test_functional_auth.py
@@ -21,7 +21,6 @@ class TestFunctionalAuth(unittest.TestCase):
         self.portal = self.layer["portal"]
         self.portal_url = self.portal.absolute_url()
         setRoles(self.portal, TEST_USER_ID, ["Manager"])
-        login(self.portal, SITE_OWNER_NAME)
         self.private_document = self.portal[
             self.portal.invokeFactory("Document", id="doc1", title="My Document")
         ]
@@ -68,12 +67,19 @@ class TestFunctionalAuth(unittest.TestCase):
         """
         Logging in via the API also grants access to the Zope root ZMI.
         """
+        app = self.layer["app"]
+        app.acl_users.plugins.users.addUser(
+            "zopeuser", "zopeuser", TEST_USER_PASSWORD,
+        )
+        app.acl_users.plugins.roles.assignRoleToPrincipal("Manager", "zopeuser")
+        transaction.commit()
+
         session = requests.Session()
         self.addCleanup(session.close)
         login_resp = session.post(
             self.portal_url + "/@login",
             headers={"Accept": "application/json"},
-            json={"login": SITE_OWNER_NAME, "password": SITE_OWNER_PASSWORD},
+            json={"login": "zopeuser", "password": TEST_USER_PASSWORD},
         )
         self.assertIn(
             "__ac",
@@ -94,11 +100,6 @@ class TestFunctionalAuth(unittest.TestCase):
         zmi_resp = session.get(
             self.layer["app"].absolute_url() + "/manage_workspace",
         )
-        # Works in the browser when running `$ bin/instance fg` in a `plone.restapi`
-        # checkout against `http://localhost:8080/manage_main` but doesn't work in the
-        # browser against the test fixture at `http://localhost:55001/manage_main`.  My
-        # guess is that there's some subtle difference in the PAS plugin configuration.
-        self.skipTest("FIXME: Works in real instance but not test fixture")
         self.assertEqual(
             zmi_resp.status_code,
             200,
