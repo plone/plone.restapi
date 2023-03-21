272223a66e4bb24658259aee365bb83b090e7b37
Katja Süss <k.suess@rohberg.ch>
2023-03-21 21:23:03 +0100

    Add tests for relations

---
+++ src/plone/restapi/tests/test_documentation_relations.py
@@ -0,0 +1,151 @@
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.app.testing import TEST_USER_ID
+from plone.restapi.testing import PLONE_RESTAPI_DX_FUNCTIONAL_TESTING
+from plone.restapi.tests.test_documentation import TestDocumentationBase
+from plone.restapi.tests.test_documentation import save_request_and_response_for_docs
+
+import transaction
+
+
+class TestRelationsDocumentation(TestDocumentationBase):
+
+    layer = PLONE_RESTAPI_DX_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        super().setUp()
+
+        self.doc1 = api.content.create(
+            container=self.portal,
+            type="Document",
+            id="document",
+            title="Test document 1",
+        )
+        api.content.transition(self.doc1, "publish")
+
+        self.doc2 = api.content.create(
+            container=self.portal,
+            type="Document",
+            id="document-2",
+            title="Test document 2",
+        )
+        api.content.transition(self.doc2, "publish")
+
+        self.doc3 = api.content.create(
+            container=self.portal,
+            type="Document",
+            id="document-3",
+            title="Test document 3",
+        )
+
+        transaction.commit()
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_documentation_relations_get(self):
+        api.relation.create(
+            source=self.doc1,
+            target=self.doc2,
+            relationship="comprisesComponentPart",
+        )
+        api.relation.create(
+            source=self.doc1,
+            target=self.doc3,
+            relationship="comprisesComponentPart",
+        )
+        api.relation.create(
+            source=self.doc1,
+            target=self.doc3,
+            relationship="relatedItems",
+        )
+        api.relation.create(
+            source=self.doc2,
+            target=self.doc1,
+            relationship="relatedItems",
+        )
+        transaction.commit()
+
+        self.assertEqual(
+            set(
+                [
+                    relationvalue.to_object
+                    for relationvalue in api.relation.get(
+                        source=self.doc1, relationship="comprisesComponentPart"
+                    )
+                ]
+            ),
+            {self.doc2, self.doc3},
+        )
+        self.assertEqual(
+            set(
+                [
+                    relationvalue.to_object
+                    for relationvalue in api.relation.get(
+                        source=self.doc1, relationship="relatedItems"
+                    )
+                ]
+            ),
+            {self.doc3},
+        )
+
+        """
+        Stats of relations
+        """
+        response = self.api_session.get(
+            "/@relations",
+        )
+        save_request_and_response_for_docs("relations_catalog_get_stats", response)
+
+        """
+        Query relations by UID
+        """
+        # relation name
+        response = self.api_session.get(
+            "/@relations?relation=comprisesComponentPart",
+        )
+        save_request_and_response_for_docs("relations_get_relationname", response)
+
+        # (sub set of relations for Anonymous)
+        self.api_session.auth = None
+        response = self.api_session.get(
+            "/@relations?relation=comprisesComponentPart",
+        )
+        save_request_and_response_for_docs(
+            "relations_get_relationname_anonymous", response
+        )
+        self.api_session.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
+
+        # source
+        response = self.api_session.get(
+            f"/@relations?source={self.doc1.UID()}",
+        )
+        save_request_and_response_for_docs("relations_get_source", response)
+
+        # (sub set of relations for Anonymous)
+        self.api_session.auth = None
+        response = self.api_session.get(
+            f"/@relations?source={self.doc1.UID()}",
+        )
+        save_request_and_response_for_docs("relations_get_source_anonymous", response)
+        self.api_session.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
+
+        # source and relation
+        response = self.api_session.get(
+            f"/@relations?source={self.doc1.UID()}&relation=comprisesComponentPart",
+        )
+        save_request_and_response_for_docs(
+            "relations_get_source_and_relation", response
+        )
+
+        # target
+        response = self.api_session.get(
+            f"/@relations?target={self.doc1.UID()}",
+        )
+        save_request_and_response_for_docs("relations_get_target", response)
+
+        """
+        TODO Query relations by path
+        """
---
+++ src/plone/restapi/tests/test_relations_get.py
@@ -0,0 +1,67 @@
+from plone import api
+from plone.app.testing import login
+from plone.app.testing import setRoles
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.app.testing import TEST_USER_ID
+from plone.app.textfield.value import RichTextValue
+from plone.namedfile.file import NamedBlobImage
+from plone.restapi.testing import PLONE_RESTAPI_DX_FUNCTIONAL_TESTING
+from Products.CMFCore.utils import getToolByName
+from z3c.relationfield import RelationValue
+from zope.component import getUtility
+from zope.intid.interfaces import IIntIds
+
+import os
+import requests
+import transaction
+import unittest
+
+
+class TestRelationsGet(unittest.TestCase):
+
+    layer = PLONE_RESTAPI_DX_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.app = self.layer["app"]
+        self.portal = self.layer["portal"]
+        self.request = self.layer["request"]
+        self.portal_url = self.portal.absolute_url()
+        setRoles(self.portal, TEST_USER_ID, ["Member"])
+        login(self.portal, SITE_OWNER_NAME)
+        self.doc1 = api.content.create(
+            container=self.portal,
+            type="Document",
+            id="doc1",
+            title="Document 1",
+        )
+        self.doc2 = api.content.create(
+            container=self.portal,
+            type="Document",
+            id="doc2",
+            title="Document 2",
+        )
+        self.doc3 = api.content.create(
+            container=self.portal,
+            type="Document",
+            id="doc3",
+            title="Document 3",
+        )
+        api.content.transition(self.doc1, "publish")
+        api.content.transition(self.doc2, "publish")
+        # doc3 stays private
+
+        transaction.commit()
+
+    def test_get_content_include_items(self):
+        response = requests.get(
+            self.portal.absolute_url() + "/@relations",
+            headers={"Accept": "application/json"},
+            auth=(SITE_OWNER_NAME, SITE_OWNER_PASSWORD),
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("broken", response.json())
+
+
+# TODO test relations
