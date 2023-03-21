17f541597c9a933cabf1d8148705320e1a284213
Katja Süss <k.suess@rohberg.ch>
2023-03-21 21:22:23 +0100

    @relations: onlyBroken: Return broken relations

--- src/plone/restapi/services/relations/get.py
+++ src/plone/restapi/services/relations/get.py
@@ -1,12 +1,11 @@
+from plone import api
 from AccessControl.SecurityManagement import getSecurityManager
 from collections import defaultdict
 from plone.restapi.interfaces import ISerializeToJsonSummary
 from plone.restapi.serializer.converters import json_compatible
 from plone.restapi.services import Service
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.relationhelper import get_relations_stats
 from zc.relation.interfaces import ICatalog
-from zExceptions import BadRequest, Unauthorized
+from zExceptions import Unauthorized
 from zope.component import getMultiAdapter
 from zope.component import getUtility
 from zope.component import queryUtility
@@ -15,6 +14,8 @@ from zope.intid.interfaces import IIntIds
 from zope.interface import alsoProvides
 
 import plone.protect.interfaces
+
+MAX = 2500
 
 try:
     from Products.CMFPlone.relationhelper import get_relations_stats
@@ -41,7 +42,10 @@ def make_summary(obj, request):
     metadata_fields.append("UID")
     request.form["metadata_fields"] = list(set(metadata_fields))
     summary = getMultiAdapter((obj, request), ISerializeToJsonSummary)()
-    return json_compatible(summary)
+    summary = json_compatible(summary)
+    ("image_scales" in summary) and summary.pop("image_scales", None)
+    ("image_field" in summary) and summary.pop("image_field", None)
+    return summary
 
 
 def get_relations(
@@ -50,6 +54,7 @@ def get_relations(
     relationship=None,
     request=None,
     unrestricted=False,
+    onlyBroken=False,
     max=None,
 ):
     results = defaultdict(list)
@@ -73,27 +78,44 @@ def get_relations(
 
     relations = relation_catalog.findRelations(query)
     if max:
-        relations = relations[: max - 1]
-    for relation in relations:
+        try:
+            max = int(max)
+        except TypeError as e:
+            raise ValueError(str(e))
+    for count, relation in enumerate(relations):
+        if max and count > max:
+            break
         if relation.isBroken():
-            continue
+            if not onlyBroken:
+                continue
+        else:
+            if onlyBroken:
+                continue
 
         source_obj = relation.from_object
         target_obj = relation.to_object
 
         if not unrestricted:
-            can_view = checkPermission("View", source_obj) and checkPermission(
-                "View", target_obj
+            can_view = (not source_obj or checkPermission("View", source_obj)) and (
+                not target_obj or checkPermission("View", target_obj)
             )
             if not can_view:
                 continue
 
-        results[relation.from_attribute].append(
-            {
-                "source": make_summary(source_obj, request),
-                "target": make_summary(target_obj, request),
-            }
-        )
+        if onlyBroken:
+            results[relation.from_attribute].append(
+                [
+                    source_obj and source_obj.absolute_url() or "",
+                    target_obj and target_obj.absolute_url() or "",
+                ]
+            )
+        else:
+            results[relation.from_attribute].append(
+                {
+                    "source": make_summary(source_obj, request),
+                    "target": make_summary(target_obj, request),
+                }
+            )
     return results
 
 
@@ -106,15 +128,26 @@ def relation_stats():
         raise ImportError
 
 
+def getBrokenRelationNames():
+    relation_catalog = queryUtility(ICatalog)
+    if relation_catalog is None:
+        return []
+
+    relations = relation_stats()
+    return relations["broken"] and relations["broken"].keys() or []
+
+
 class GetRelations(Service):
     """Get relations or stats
 
         source: UID of content item
         target: UID of content item
         relation: name of a relation
+        onlyBroken: boolean: dictionary with broken relations per relation type
+        max: integer: maximum of results
 
     Returns:
-        dictionary of relations grouped by relation name
+        stats if no parameter, else relations
     """
 
     def __init__(self, context, request):
@@ -122,7 +155,6 @@ class GetRelations(Service):
         self.sm = getSecurityManager()
 
     def reply(self):
-
         # Disable CSRF protection
         if "IDisableCSRFProtection" in dir(plone.protect.interfaces):
             alsoProvides(self.request, plone.protect.interfaces.IDisableCSRFProtection)
@@ -131,7 +163,10 @@ class GetRelations(Service):
         target = self.request.get("target", None)
         relationship = self.request.get("relation", None)
         rebuild = self.request.get("rebuild", False)
+        max = self.request.get("max", False)
+        onlyBroken = self.request.get("onlyBroken", False)
 
+        # Rebuild relations with or without regenerating intids
         if rebuild:
             if rebuild_relations:
                 flush = self.request.get("flush", None) and True or False
@@ -157,6 +192,20 @@ class GetRelations(Service):
                     )
                 )
 
+        # Get broken relations for all relation types
+        if onlyBroken:
+            data = {}
+            relationNames = getBrokenRelationNames()
+            for relationName in relationNames:
+                foo = get_relations(relationship=relationName, onlyBroken=True)
+                data.update(foo)
+            return {
+                "@id": f'{self.request["SERVER_URL"]}{self.request.environ["REQUEST_URI"]}',
+                "items": data,
+                "items_total": dict([(el, len(data[el])) for el in data]),
+            }
+
+        # Stats
         if not source and not target and not relationship:
             try:
                 stats = relation_stats()
@@ -175,19 +224,23 @@ class GetRelations(Service):
             except Unauthorized:
                 return self.reply_no_content(status=401)
 
-        catalog = getToolByName(self.context, "portal_catalog")
+        # Query relations
         if source:
-            brains = catalog(UUID=source)
-            if brains:
-                source = brains[0].getObject()
+            if source[0:1] == "/":
+                print("Find source by path")
+                source = api.content.get(path=source)
             else:
+                print("Find source by UID")
+                source = api.content.get(UID=source)
+            if not source:
                 return self.reply_no_content(status=404)
 
         if target:
-            brains = catalog(UUID=target)
-            if brains:
-                target = brains[0].getObject()
+            if target[0:1] == "/":
+                target = api.content.get(path=target)
             else:
+                target = api.content.get(UID=target)
+            if not target:
                 return self.reply_no_content(status=404)
 
         data = get_relations(
@@ -195,6 +248,7 @@ class GetRelations(Service):
             target=target,
             relationship=relationship,
             # unrestricted=True,
+            max=max,
             request=self.request,
         )
 
