100d3040fd0dd79bad39e3067d98145989a12bac
Jens W. Klein <jk@kleinundpartner.at>
2023-03-30 10:35:27 +0200

    no special handling in plone.app.uuid 2.x needed

--- src/plone/restapi/serializer/utils.py
+++ src/plone/restapi/serializer/utils.py
@@ -1,20 +1,9 @@
+from plone.app.uuid.utils import uuidToObject
+from plone.app.uuid.utils import uuidToURL
 from plone.restapi.interfaces import IObjectPrimaryFieldTarget
 from zope.component import queryMultiAdapter
 
 import re
-
-
-try:
-    from plone.app.uuid.utils import uuidToObject
-    from plone.app.uuid.utils import uuidToURL
-
-except ImportError:
-    # Plone 5.2
-    from plone.outputfilters.browser.resolveuid import uuidToObject as old_uuidToObject
-    from plone.outputfilters.browser.resolveuid import uuidToURL
-
-    def uuidToObject(uid, unrestricted=False):
-        return old_uuidToObject(uid)
 
 
 RESOLVEUID_RE = re.compile("^[./]*resolve[Uu]id/([^/]*)/?(.*)$")
@@ -34,7 +23,7 @@ def uid_to_url(path):
     if suffix:
         href += "/" + suffix
     else:
-        target_object = uuidToObject(uid, unrestricted=True)
+        target_object = uuidToObject(uid)
         if target_object:
             adapter = queryMultiAdapter(
                 (target_object, target_object.REQUEST),
