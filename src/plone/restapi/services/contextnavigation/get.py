plone/plone.restapi

commit 04e69827e9f8f935b0091a50096f780d2363b13c

Add portal_type title (#1355)
* Add type title to content serializer

* Add portal_type title to serializers

* Update tests to include the new 'type' field

* Translate type

* Add changelog

* ref #1322: add  portal_type id as '@type' to types GET api

* Renamed type to type_name

* Rename type to type_name in tests

* also support portal_type in parent field in dx contents, fix tests

* fix(): rename method get_portal_type, remove type_name explicit assignment

* refactor(summarySerializer): set type_name in defaultSummarySerializer

* refactor(utils): use lookup_fti for getting DX fti efficiently

* fix(): remove unused imports

* fix: unused import

* Update news/1355.feature

Co-authored-by: David Glick <david@glicksoftware.com>

* fix(code-analysis): re-format to add whitespace

* rename(type_name) -> type-title

* Update src/plone/restapi/services/types/get.py

Co-authored-by: David Glick <david@glicksoftware.com>

* change(types): @type -> id, fix tests

* Refactor get_portal_type_title request getter

* Cleanup

---------

Co-authored-by: Alin Voinea <contact@avoinea.com>
Co-authored-by: Nilesh <nileshgulia@gmail.com>
Co-authored-by: David Glick <david@glicksoftware.com>

src/plone/restapi/services/contextnavigation/get.py
@@ -188,7 +188,6 @@ class ContextNavigation:
 
 class NavigationPortletRenderer:
     def __init__(self, context, request, data):
-
         self.context = context
         self.request = request
         self.data = data
@@ -251,7 +250,7 @@ class NavigationPortletRenderer:
             # Go to the item /view we have chosen as root item
             return nav_root.absolute_url()
 
-    def root_type_name(self):
+    def root_type_title(self):
         root = self.getNavRoot()
         return queryUtility(IIDNormalizer).normalize(root.portal_type)
 

src/plone/restapi/services/types/get.py
@@ -72,6 +72,7 @@ class TypesInfo:
         result["types"] = [
             {
                 "@id": f"{portal_url}/@types/{fti.getId()}",
+                "id": fti.getId(),
                 "title": translate(fti.Title(), context=self.request),
                 "addable": fti.getId() in allowed_types if can_add else False,
                 "immediately_addable": fti.getId() in immediately_types

src/plone/restapi/serializer/dxcontent.py
@@ -16,6 +16,7 @@ from plone.restapi.serializer.converters import json_compatible
 from plone.restapi.serializer.expansion import expandable_elements
 from plone.restapi.serializer.nextprev import NextPrevious
 from plone.restapi.services.locking import lock_info
+from plone.restapi.serializer.utils import get_portal_type_title
 from plone.rfc822.interfaces import IPrimaryFieldInfo
 from plone.supermodel.utils import mergedTaggedValueDict
 from Products.CMFCore.utils import getToolByName
@@ -62,11 +63,13 @@ class SerializeToJson:
         parent_summary = getMultiAdapter(
             (parent, self.request), ISerializeToJsonSummary
         )()
+
         result = {
             # '@context': 'http://www.w3.org/ns/hydra/context.jsonld',
             "@id": obj.absolute_url(),
             "id": obj.id,
             "@type": obj.portal_type,
+            "type_title": get_portal_type_title(obj.portal_type),
             "parent": parent_summary,
             "created": json_compatible(obj.created()),
             "modified": json_compatible(obj.modified()),
@@ -101,7 +104,6 @@ class SerializeToJson:
             read_permissions = mergedTaggedValueDict(schema, READ_PERMISSIONS_KEY)
 
             for name, field in getFields(schema).items():
-
                 if not self.check_permission(read_permissions.get(name), obj):
                     continue
 
@@ -203,7 +205,6 @@ class DexterityObjectPrimaryFieldTarget:
             read_permissions = mergedTaggedValueDict(schema, READ_PERMISSIONS_KEY)
 
             for name, field in getFields(schema).items():
-
                 if not self.check_permission(read_permissions.get(name), self.context):
                     continue
 

src/plone/restapi/serializer/nextprev.py
@@ -3,6 +3,7 @@ from Acquisition import aq_inner
 from Acquisition import aq_parent
 from plone.app.dexterity.behaviors.nextprevious import NextPreviousBase
 from plone.registry.interfaces import IRegistry
+from plone.restapi.serializer.utils import get_portal_type_title
 from zope.component import getUtility
 
 
@@ -40,6 +41,7 @@ class NextPrevious:
         return {
             "@id": data["url"].lstrip("/view"),
             "@type": data["portal_type"],
+            "type_title": get_portal_type_title(data.get("portal_type")),
             "title": data["title"],
             "description": data["description"],
         }
@@ -56,6 +58,7 @@ class NextPrevious:
         return {
             "@id": data["url"].lstrip("/view"),
             "@type": data["portal_type"],
+            "type_title": get_portal_type_title(data.get("portal_type")),
             "title": data["title"],
             "description": data["description"],
         }

src/plone/restapi/serializer/site.py
@@ -11,6 +11,7 @@ from plone.restapi.serializer.blocks import apply_block_serialization_transforms
 from plone.restapi.serializer.converters import json_compatible
 from plone.restapi.serializer.expansion import expandable_elements
 from plone.restapi.services.locking import lock_info
+from plone.restapi.serializer.utils import get_portal_type_title
 from plone.supermodel.utils import mergedTaggedValueDict
 from Products.CMFCore.utils import getToolByName
 from zope.component import adapter
@@ -62,6 +63,7 @@ class SerializeSiteRootToJson:
             "@id": self.context.absolute_url(),
             "id": self.context.id,
             "@type": "Plone Site",
+            "type_title": get_portal_type_title("Plone Site"),
             "title": self.context.Title(),
             "parent": {},
             "is_folderish": True,
@@ -81,7 +83,6 @@ class SerializeSiteRootToJson:
                 read_permissions = mergedTaggedValueDict(schema, READ_PERMISSIONS_KEY)
 
                 for name, field in getFields(schema).items():
-
                     if not self.check_permission(
                         read_permissions.get(name), self.context
                     ):

src/plone/restapi/serializer/summary.py
@@ -4,6 +4,7 @@ from plone.restapi.deserializer import json_body
 from plone.restapi.interfaces import IJSONSummarySerializerMetadata
 from plone.restapi.interfaces import ISerializeToJsonSummary
 from plone.restapi.serializer.converters import json_compatible
+from plone.restapi.serializer.utils import get_portal_type_title
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.WorkflowCore import WorkflowException
 from zope.component import adapter
@@ -15,13 +16,7 @@ from zope.interface import Interface
 @implementer(IJSONSummarySerializerMetadata)
 class JSONSummarySerializerMetadata:
     def default_metadata_fields(self):
-        return {
-            "@id",
-            "@type",
-            "description",
-            "review_state",
-            "title",
-        }
+        return {"@id", "@type", "description", "review_state", "title", "type_title"}
 
     def field_accessors(self):
         return {
@@ -95,7 +90,10 @@ class DefaultJSONSummarySerializer:
             if field.startswith("_") or field in self.blocklisted_attributes:
                 continue
             accessor = self.field_accessors.get(field, field)
-            value = getattr(obj, accessor, None)
+            if field == "type_title":
+                value = get_portal_type_title(self.context.portal_type)
+            else:
+                value = getattr(obj, accessor, None)
             try:
                 if callable(value):
                     value = value()
@@ -140,6 +138,7 @@ class SiteRootJSONSummarySerializer:
             {
                 "@id": self.context.absolute_url(),
                 "@type": self.context.portal_type,
+                "type_title": get_portal_type_title(self.context.portal_type),
                 "title": self.context.title,
                 "description": self.context.description,
             }

src/plone/restapi/serializer/utils.py
@@ -1,6 +1,9 @@
+from plone.dexterity.schema import lookup_fti
 from plone.app.uuid.utils import uuidToCatalogBrain
 from plone.restapi.interfaces import IObjectPrimaryFieldTarget
 from zope.component import queryMultiAdapter
+from zope.i18n import translate
+from zope.globalrequest import getRequest
 
 import re
 
@@ -38,3 +41,11 @@ def uid_to_url(path):
         if a_href:
             return a_href
     return href
+
+
+def get_portal_type_title(portal_type):
+    fti = lookup_fti(portal_type)
+    request = getRequest()
+    if request:
+        return translate(getattr(fti, "Title", lambda: portal_type)(), context=request)
+    return getattr(fti, "Title", lambda: portal_type)()

src/plone/restapi/tests/http-examples/batching.resp
@@ -15,35 +15,40 @@ Content-Type: application/json
             "@type": "Folder",
             "description": "",
             "review_state": "private",
-            "title": "Folder"
+            "title": "Folder",
+            "type_title": "Folder"
         },
         {
             "@id": "http://localhost:55001/plone/folder/doc-1",
             "@type": "Document",
             "description": "",
             "review_state": "private",
-            "title": "Document 1"
+            "title": "Document 1",
+            "type_title": "Page"
         },
         {
             "@id": "http://localhost:55001/plone/folder/doc-2",
             "@type": "Document",
             "description": "",
             "review_state": "private",
-            "title": "Document 2"
+            "title": "Document 2",
+            "type_title": "Page"
         },
         {
             "@id": "http://localhost:55001/plone/folder/doc-3",
             "@type": "Document",
             "description": "",
             "review_state": "private",
-            "title": "Document 3"
+            "title": "Document 3",
+            "type_title": "Page"
         },
         {
             "@id": "http://localhost:55001/plone/folder/doc-4",
             "@type": "Document",
             "description": "",
             "review_state": "private",
-            "title": "Document 4"
+            "title": "Document 4",
+            "type_title": "Page"
         }
     ],
     "items_total": 8

commit 088fb18d230d4c795413f87bc2fcf786d2d18827

Fix search/replace regression in #1355

src/plone/restapi/services/contextnavigation/get.py
@@ -250,7 +250,7 @@ class NavigationPortletRenderer:
             # Go to the item /view we have chosen as root item
             return nav_root.absolute_url()
 
-    def root_type_title(self):
+    def root_type_name(self):
         root = self.getNavRoot()
         return queryUtility(IIDNormalizer).normalize(root.portal_type)
 