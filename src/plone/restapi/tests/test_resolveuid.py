36e67bd6c50053c71afdc34ff21fe23fdcd848a3
Andrea Cecchi <andrea.cecchi85@gmail.com>
2023-09-03 07:12:12 +0200

    Improve RESOLVEUID_RE regexp to catch also paths generated by Link content-types (#1700)
    * Improve regexp to catch also paths generated by Link content-types
    
    * add changelog

--- src/plone/restapi/tests/test_resolveuid.py
+++ src/plone/restapi/tests/test_resolveuid.py
@@ -24,7 +24,6 @@ import transaction
 
 
 class TestBlocksResolveUIDFunctional(TestCase):
-
     layer = PLONE_RESTAPI_BLOCKS_FUNCTIONAL_TESTING
 
     def setUp(self):
@@ -698,3 +697,9 @@ class TestBlocksResolveUID(TestCase):
             ]["url"],
             self.doc2.absolute_url() + "/view",
         )
+
+    def test_resolveuid_handle_link_style_formats(self):
+        uid = IUUID(self.doc2)
+        blocks = {"aaa": {"@type": "foo", "url": f"/plone/resolveuid/{uid}"}}
+        value = self.serialize("blocks", blocks)
+        self.assertEqual(value["aaa"]["url"], self.doc2.absolute_url())
