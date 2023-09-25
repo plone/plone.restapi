c16f5921391595db4bb9c6035014d1d4617829b0
wesleybl <wesleybl@gmail.com>
2023-09-25 13:04:15 -0300

    Add can_assign and can_assign_add keys to the roles endpoint
    This is to inform the front-end whether role assignment should be
    allowed or not. It was necessary to create the can_assign_add key,
    because a user can only create another with the roles they have. But in
    editing he can assign other roles.

--- src/plone/restapi/services/roles/get.py
+++ src/plone/restapi/services/roles/get.py
@@ -1,19 +1,44 @@
+from AccessControl import getSecurityManager
 from Acquisition import aq_inner
 from plone.restapi.services import Service
+from Products.CMFCore.permissions import ManagePortal
 from Products.CMFCore.utils import getToolByName
 from zope.i18n import translate
 
 
 class RolesGet(Service):
+    @property
+    def is_zope_manager(self):
+        return getSecurityManager().checkPermission(ManagePortal, self.context)
+
+    def get_user_roles(self):
+        membership = getToolByName(self.context, "portal_membership")
+        member = membership.getAuthenticatedMember()
+        return member.getRoles()
+
+    def can_assign(self, is_zope_manager, _id):
+        if is_zope_manager:
+            return True
+        return _id != "Manager"
+
+    def can_assign_add(self, is_zope_manager, user_roles, _id):
+        if is_zope_manager:
+            return True
+        return _id in user_roles
+
     def reply(self):
         pmemb = getToolByName(aq_inner(self.context), "portal_membership")
         roles = [r for r in pmemb.getPortalRoles() if r != "Owner"]
+        is_zope_manager = self.is_zope_manager
+        user_roles = self.get_user_roles()
         return [
             {
                 "@type": "role",
                 "@id": f"{self.context.absolute_url()}/@roles/{r}",
                 "id": r,
                 "title": translate(r, context=self.request, domain="plone"),
+                "can_assign": self.can_assign(is_zope_manager, r),
+                "can_assign_add": self.can_assign_add(is_zope_manager, user_roles, r),
             }
             for r in roles
         ]
---
+++ src/plone/restapi/tests/test_services_roles.py
@@ -0,0 +1,194 @@
+from plone import api
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.restapi.testing import PLONE_RESTAPI_DX_FUNCTIONAL_TESTING
+from plone.restapi.testing import RelativeSession
+
+import transaction
+import unittest
+
+
+class TestRolesGet(unittest.TestCase):
+
+    layer = PLONE_RESTAPI_DX_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        self.portal_url = self.portal.absolute_url()
+
+        self.api_session = RelativeSession(self.portal_url, test=self)
+        self.api_session.headers.update({"Accept": "application/json"})
+        self.api_session.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
+
+    def tearDown(self):
+        self.api_session.close()
+
+    def set_siteadm(self):
+        siteadm_username = "siteadm"
+        siteadm_password = "siteadmpassword"
+        api.user.create(
+            email="siteadm@example.com",
+            roles=["Site Administrator"],
+            username=siteadm_username,
+            password=siteadm_password,
+        )
+        self.api_session = RelativeSession(self.portal_url, test=self)
+        self.api_session.headers.update({"Accept": "application/json"})
+        self.api_session.auth = (siteadm_username, siteadm_password)
+        transaction.commit()
+
+    def test_roles_endpoint_lists_roles(self):
+        response = self.api_session.get("/@roles")
+
+        expected = (
+            {
+                "@id": self.portal_url + "/@roles/Contributor",
+                "@type": "role",
+                "id": "Contributor",
+                "title": "Contributor",
+                "can_assign": True,
+                "can_assign_add": True,
+            },
+            {
+                "@id": self.portal_url + "/@roles/Editor",
+                "@type": "role",
+                "id": "Editor",
+                "title": "Editor",
+                "can_assign": True,
+                "can_assign_add": True,
+            },
+            {
+                "@id": self.portal_url + "/@roles/Member",
+                "@type": "role",
+                "id": "Member",
+                "title": "Member",
+                "can_assign": True,
+                "can_assign_add": True,
+            },
+            {
+                "@id": self.portal_url + "/@roles/Reader",
+                "@type": "role",
+                "id": "Reader",
+                "title": "Reader",
+                "can_assign": True,
+                "can_assign_add": True,
+            },
+            {
+                "@id": self.portal_url + "/@roles/Reviewer",
+                "@type": "role",
+                "id": "Reviewer",
+                "title": "Reviewer",
+                "can_assign": True,
+                "can_assign_add": True,
+            },
+            {
+                "@id": self.portal_url + "/@roles/Site Administrator",
+                "@type": "role",
+                "id": "Site Administrator",
+                "title": "Site Administrator",
+                "can_assign": True,
+                "can_assign_add": True,
+            },
+            {
+                "@id": self.portal_url + "/@roles/Manager",
+                "@type": "role",
+                "id": "Manager",
+                "title": "Manager",
+                "can_assign": True,
+                "can_assign_add": True,
+            },
+        )
+        result = response.json()
+        self.assertEqual(len(expected), len(result))
+        for item in result:
+            self.assertIn(item, expected)
+
+    def test_siteadm_roles_endpoint_lists_roles(self):
+        self.set_siteadm()
+        response = self.api_session.get("/@roles")
+
+        expected = (
+            {
+                "@id": self.portal_url + "/@roles/Contributor",
+                "@type": "role",
+                "id": "Contributor",
+                "title": "Contributor",
+                "can_assign": True,
+                "can_assign_add": False,
+            },
+            {
+                "@id": self.portal_url + "/@roles/Editor",
+                "@type": "role",
+                "id": "Editor",
+                "title": "Editor",
+                "can_assign": True,
+                "can_assign_add": False,
+            },
+            {
+                "@id": self.portal_url + "/@roles/Member",
+                "@type": "role",
+                "id": "Member",
+                "title": "Member",
+                "can_assign": True,
+                "can_assign_add": False,
+            },
+            {
+                "@id": self.portal_url + "/@roles/Reader",
+                "@type": "role",
+                "id": "Reader",
+                "title": "Reader",
+                "can_assign": True,
+                "can_assign_add": False,
+            },
+            {
+                "@id": self.portal_url + "/@roles/Reviewer",
+                "@type": "role",
+                "id": "Reviewer",
+                "title": "Reviewer",
+                "can_assign": True,
+                "can_assign_add": False,
+            },
+            {
+                "@id": self.portal_url + "/@roles/Site Administrator",
+                "@type": "role",
+                "id": "Site Administrator",
+                "title": "Site Administrator",
+                "can_assign": True,
+                "can_assign_add": True,
+            },
+            {
+                "@id": self.portal_url + "/@roles/Manager",
+                "@type": "role",
+                "id": "Manager",
+                "title": "Manager",
+                "can_assign": False,
+                "can_assign_add": False,
+            },
+        )
+        result = response.json()
+        self.assertEqual(len(expected), len(result))
+        for item in result:
+            self.assertIn(item, expected)
+
+    def test_roles_endpoint_translates_role_titles(self):
+        self.api_session.headers.update({"Accept-Language": "de"})
+        response = self.api_session.get("/@roles")
+        # One of the roles has changed translation in German.
+        # Reviewer used to be 'Ver\xf6ffentlichen', but is now simply Reviewer.
+        titles = {item["title"] for item in response.json()}
+        options = {"Ver\xf6ffentlichen", "Reviewer"}
+        # One of the options must match:
+        self.assertTrue(titles.intersection(options))
+        # Discard them:
+        titles = titles.difference(options)
+        self.assertEqual(
+            {
+                "Hinzuf\xfcgen",
+                "Bearbeiten",
+                "Benutzer",
+                "Ansehen",
+                "Website-Administrator",
+                "Verwalten",
+            },
+            titles,
+        )
