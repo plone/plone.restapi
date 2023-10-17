dfb352db088540b5b21830378b90f406c07d99d2
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-10-17 17:03:36 +0200

    adapt for Plone 4.3

--- src/plone/restapi/services/site/get.py
+++ src/plone/restapi/services/site/get.py
@@ -2,14 +2,21 @@
 from plone.registry.interfaces import IRegistry
 from plone.restapi.interfaces import IExpandableElement
 from plone.restapi.services import Service
-from Products.CMFPlone.interfaces import IImagingSchema
-from Products.CMFPlone.interfaces import ISiteSchema
 from Products.CMFPlone.utils import getSiteLogo
 from zope.component import adapter
 from zope.component import getMultiAdapter
 from zope.component import getUtility
 from zope.interface import implementer
 from zope.interface import Interface
+
+try:
+    from Products.CMFPlone.interfaces import IImagingSchema
+except ImportError:
+    IImagingSchema = None
+try:
+    from Products.CMFPlone.interfaces import ISiteSchema
+except ImportError:
+    ISiteSchema = None
 
 
 @implementer(IExpandableElement)
@@ -28,15 +35,27 @@ class Site:
             (self.context, self.request), name="plone_portal_state"
         )
         registry = getUtility(IRegistry)
-        site_settings = registry.forInterface(ISiteSchema, prefix="plone", check=False)
-        image_settings = registry.forInterface(
-            IImagingSchema, prefix="plone", check=False
+
+        if ISiteSchema is not None:
+            site_settings = registry.forInterface(ISiteSchema, prefix="plone", check=False)
+            result["site"].update({
+                "plone.site_logo": site_settings.site_logo and getSiteLogo() or None,
+                "plone.robots_txt": site_settings.robots_txt,
+            })
+
+        if IImagingSchema is not None:
+            image_settings = registry.forInterface(
+                IImagingSchema, prefix="plone", check=False
+            )
+            result["site"].update(
+            {
+                "plone.allowed_sizes": image_settings.allowed_sizes,
+            }
         )
+
         result["site"].update(
             {
                 "plone.site_title": portal_state.portal_title(),
-                "plone.site_logo": site_settings.site_logo and getSiteLogo() or None,
-                "plone.robots_txt": site_settings.robots_txt,
                 "plone.allowed_sizes": image_settings.allowed_sizes,
             }
         )
