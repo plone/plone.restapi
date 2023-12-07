ede2c508bc457d28b45ea67673cad9a3bfa39c69
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-12-07 08:15:20 +0100

    add available languages information to the @site endpoint

--- src/plone/restapi/services/site/get.py
+++ src/plone/restapi/services/site/get.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from plone.i18n.interfaces import ILanguageSchema
 from plone.registry.interfaces import IRegistry
 from plone.restapi.interfaces import IExpandableElement
 from plone.restapi.services import Service
@@ -32,12 +33,17 @@ class Site:
         image_settings = registry.forInterface(
             IImagingSchema, prefix="plone", check=False
         )
+        language_settings = registry.forInterface(
+            ILanguageSchema, prefix="plone", check=False
+        )
         result["site"].update(
             {
                 "plone.site_title": portal_state.portal_title(),
                 "plone.site_logo": site_settings.site_logo and getSiteLogo() or None,
                 "plone.robots_txt": site_settings.robots_txt,
                 "plone.allowed_sizes": image_settings.allowed_sizes,
+                "plone.default_language": language_settings.default_language,
+                "plone.available_languages": language_settings.available_languages,
             }
         )
 
