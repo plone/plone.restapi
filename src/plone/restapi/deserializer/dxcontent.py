63ee85d6104525cfa71416a255f34af89527aa36
Johannes Raggam <thetetet@gmail.com>
2024-01-11 17:54:50 +0100

    Dexterity deserializer: Allow to ignore validation errors.

--- src/plone/restapi/deserializer/dxcontent.py
+++ src/plone/restapi/deserializer/dxcontent.py
@@ -24,6 +24,11 @@ from zope.schema import getFields
 from zope.schema.interfaces import ValidationError
 from zope.security.interfaces import IPermission
 
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 
 @implementer(IDeserializeFromJson)
 @adapter(IDexterityContent, Interface)
@@ -37,7 +42,12 @@ class DeserializeFromJson(OrderingMixin):
         self.modified = {}
 
     def __call__(
-        self, validate_all=False, data=None, create=False, mask_validation_errors=True
+        self,
+        validate_all=False,
+        data=None,
+        create=False,
+        mask_validation_errors=True,
+        ignore_errors=False,
     ):  # noqa: ignore=C901
         if data is None:
             data = json_body(self.request)
@@ -60,7 +70,10 @@ class DeserializeFromJson(OrderingMixin):
                     error["error"] = "ValidationError"
             for error in errors:
                 error["message"] = translate(error["message"], context=self.request)
-            raise BadRequest(errors)
+            if ignore_errors:
+                logger.warn("Ignoring validation errors: %s", errors)
+            else:
+                raise BadRequest(errors)
 
         # We'll set the layout after the validation and even if there
         # are no other changes.
