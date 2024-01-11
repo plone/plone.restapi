1a8cc59a1a71f777c86d8aa7c4ade3d9f197f3c5
Johannes Raggam <thetetet@gmail.com>
2024-01-11 01:33:59 +0100

    black and docformatter.

--- src/plone/restapi/deserializer/dxcontent.py
+++ src/plone/restapi/deserializer/dxcontent.py
@@ -39,7 +39,6 @@ class DeserializeFromJson(OrderingMixin):
     def __call__(
         self, validate_all=False, data=None, create=False, mask_validation_errors=True
     ):  # noqa: ignore=C901
-
         if data is None:
             data = json_body(self.request)
 
@@ -171,8 +170,9 @@ class DeserializeFromJson(OrderingMixin):
         return schema_data, errors
 
     def mark_field_as_changed(self, schema, fieldname):
-        """Collect the names of the modified fields. Use prefixed name because
-        z3c.form does so.
+        """Collect the names of the modified fields.
+
+        Use prefixed name because z3c.form does so.
         """
 
         prefixed_name = schema.__name__ + "." + fieldname
