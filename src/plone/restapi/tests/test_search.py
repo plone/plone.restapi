e51b6aa55ecb6fdb7e09b615d25bafdef3cc8f28
Teodor <teodorvoicu87@gmail.com>
2024-10-30 16:12:31 +0200

    run black to format code

--- src/plone/restapi/tests/test_search.py
+++ src/plone/restapi/tests/test_search.py
@@ -153,50 +153,26 @@ class TestSearchFunctional(unittest.TestCase):
 
     def test_search_with_parentheses(self):
         query = {"SearchableText": "("}
-        response = self.api_session.get(
-            "/@search", 
-            params=query
-            )
+        response = self.api_session.get("/@search", params=query)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(
-            response.status_code, 
-            200
-            )
-        self.assertEqual(
-            response.json(),
-            [], 
-            "Expected no items for query with only parentheses"
-            )
+            response.json(), [], "Expected no items for query with only parentheses"
+        )
 
         query = {"SearchableText": ")"}
-        response = self.api_session.get(
-            "/@search", 
-            params=query
-            )
+        response = self.api_session.get("/@search", params=query)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(
-            response.status_code, 
-            200
-            )
-        self.assertEqual(
-            response.json(), 
-            [], 
-            "Expected no items for query with only parentheses"
-            )
+            response.json(), [], "Expected no items for query with only parentheses"
+        )
 
         query = {"SearchableText": "lorem(ipsum)"}
-        response = self.api_session.get(
-            "/@search", 
-            params=query
-            )
-        self.assertEqual(
-            response.status_code, 
-            200
-            )
+        response = self.api_session.get("/@search", params=query)
+        self.assertEqual(response.status_code, 200)
         items = [item["title"] for item in response.json().get("items", [])]
         self.assertIn(
-            "Lorem Ipsum", 
-            items, 
-            "Expected 'Lorem Ipsum' to be found in search results"
-            )
+            "Lorem Ipsum", items, "Expected 'Lorem Ipsum' to be found in search results"
+        )
 
     def test_search_in_vhm(self):
         # Install a Virtual Host Monster
