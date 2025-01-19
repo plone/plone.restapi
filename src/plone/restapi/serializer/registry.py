7b3f3601b4068118e138416537f1ffb141a46d71
Faakhir30 <zahidfaakhir@gmail.com>
2025-01-20 02:54:35 +0500

    refactor serializer.

--- src/plone/restapi/serializer/registry.py
+++ src/plone/restapi/serializer/registry.py
@@ -8,33 +8,48 @@ from zope.interface import implementer
 from zope.publisher.interfaces import IRequest
 from zope.interface import Interface
 
+class SerializeRegistryMixin:
+    def serialize(self):
+        batch = HypermediaBatch(self.request, list(self.records.keys()))
+        results = {
+            "@id": batch.canonical_url,
+            "items_total": batch.items_total,
+            "batching": batch.links if batch.links else {},
+            "items": [
+                self.make_item(key) for key in batch
+            ]
+        }
+        return results
+
+    def make_item(self, key):
+        record = self.records[key]
+        schema = getMultiAdapter(
+            (record.field, record, self.request), IJsonSchemaProvider
+        )
+        return {
+            "name": key,
+            "value": self.records[key],
+            "schema": {"properties": schema.get_schema()},
+        }
 
 @implementer(ISerializeToJson)
 @adapter(IRegistry, IRequest, Interface)
-class SerializeRegistryToJson:
-    def __init__(self, registry, request, records=None):
+class SerializeRegistryToJsonWithFilters(SerializeRegistryMixin):
+    def __init__(self, registry, request, records):
         self.registry = registry
         self.request = request
-        self.records = records or registry.records
+        self.records = records
+    def __call__(self):
+        return self.serialize()
+
+
+@implementer(ISerializeToJson)
+@adapter(IRegistry, IRequest)
+class SerializeRegistryToJson(SerializeRegistryMixin):
+    def __init__(self, registry, request):
+        self.registry = registry
+        self.request = request
+        self.records = registry.records
 
     def __call__(self):
-        batch = HypermediaBatch(self.request, list(self.records.keys()))
-
-        results = {}
-        results["@id"] = batch.canonical_url
-        results["items_total"] = batch.items_total
-        if batch.links:
-            results["batching"] = batch.links
-
-        def make_item(key):
-            record = self.records[key]
-            schema = getMultiAdapter(
-                (record.field, record, self.request), IJsonSchemaProvider
-            )
-            data = {"name": key, "value": self.registry[key]}
-            __traceback_info__ = (record, record.field, schema)
-            data["schema"] = {"properties": schema.get_schema()}
-            return data
-
-        results["items"] = [make_item(key) for key in batch]
-        return results
+        return self.serialize()
