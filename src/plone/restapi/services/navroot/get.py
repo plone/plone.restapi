0010890dd599986dd3887fe27236b4cc89379c06
Paul Grunewald <paul.grunewald@tu-dresden.de>
2025-05-29 11:45:25 +0200

    Make @navroot also work with old/custom ISerializeToJson implementations

--- src/plone/restapi/services/navroot/get.py
+++ src/plone/restapi/services/navroot/get.py
@@ -25,10 +25,17 @@ class Navroot:
         # We need to unset expansion here, otherwise we get infinite recursion
         self.request.form["expand"] = ""
 
-        result["navroot"]["navroot"] = getMultiAdapter(
-            (portal_state.navigation_root(), self.request),
-            ISerializeToJson,
-        )(include_items=False, include_expansion=False)
+        try:
+            result["navroot"]["navroot"] = getMultiAdapter(
+                (portal_state.navigation_root(), self.request),
+                ISerializeToJson,
+            )(include_items=False, include_expansion=False)
+        except TypeError:
+            # Handle adapters that do not support keyword args gracefully.
+            result["navroot"]["navroot"] = getMultiAdapter(
+                (portal_state.navigation_root(), self.request),
+                ISerializeToJson,
+            )()
 
         return result
 
