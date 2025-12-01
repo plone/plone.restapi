84019a79e19acc01b344c5158ef99298034f7094
nithin01010 <153373802+nithin01010@users.noreply.github.com>
2025-12-02 00:32:12 +0530

    generalized the boolean values

--- src/plone/restapi/tests/test_boolean_value.py
+++ src/plone/restapi/tests/test_boolean_value.py
@@ -39,4 +39,4 @@ class TestBooleanValue(unittest.TestCase):
         self.assertFalse(boolean_value("0"))
 
     def test_true_other_value(self):
-        self.assertFalse(boolean_value("foobar"))
+        self.assertTrue(boolean_value("foobar"))
