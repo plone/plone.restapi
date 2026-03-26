3f2b4334b9fd002622371e5c2aca37337ecd0209
Maurits van Rees <maurits@vanrees.org>
2026-03-26 12:29:18 +0100

    Declare `plone.api` as runtime dependency instead of only a test dependency.
    Fixes https://github.com/plone/plone.restapi/issues/2007

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ TEST_REQUIRES = [
     "plone.app.multilingual",
     "plone.app.testing",
     "plone.app.upgrade",
-    "plone.api",
     "plone.rest>=3.0.1",
     "requests",
 ]
@@ -71,6 +70,7 @@ setup(
     install_requires=[
         "packaging",
         "python-dateutil",
+        "plone.api",
         "plone.rest",  # json renderer moved to plone.restapi
         "plone.schema>=1.2.1",  # new/fixed json field
         "Products.CMFPlone>=5.2",
